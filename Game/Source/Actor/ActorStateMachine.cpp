dfafc1fbc0cded70daa1998308dfab8c342170c5
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-02-02 21:01:43 +0900

    IStateクラス、ActorStateMachineクラスを追加
    ステートを作るときはIStateを継承してください。
    ステートマシンを作るときはActorStateMachineを継承してください。

---
+++ Game/Source/Actor/ActorStateMachine.cpp
@@ -0,0 +1,62 @@
+﻿#include "stdafx.h"
+#include "ActorStateMachine.h"
+
+
+
+
+namespace app
+{
+	namespace actor
+	{
+		IState::IState()
+			: m_stateMachine(nullptr)
+		{
+		}
+
+
+
+		/***************************************/
+
+
+		void ActorStateMachine::Update()
+		{
+			// ステートを変更する
+			ChangeState();
+
+			// 現在のステートを更新する
+			if (m_currentState) m_currentState->Update();
+		}
+
+
+		void ActorStateMachine::ChangeState()
+		{
+			m_nextState = GetChangeState();
+
+			if (m_nextState && m_currentState == m_nextState)
+			{
+				m_currentState->Exit();
+				m_currentState = m_nextState;
+				m_currentState->Enter();
+				m_nextState = nullptr;
+			}
+		}
+
+
+		IState* ActorStateMachine::FindState(const uint8_t stateID)
+		{
+			const auto& it = m_stateMap.find(stateID);
+
+			if (it == m_stateMap.end()) return nullptr;
+
+			return it->second.get();
+		}
+
+
+		ActorStateMachine::ActorStateMachine()
+			: m_currentState(nullptr)
+			, m_nextState(nullptr)
+		{
+			m_stateMap.clear();
+		}
+	}
+}
---
+++ Game/Source/Actor/ActorStateMachine.h
@@ -0,0 +1,101 @@
+﻿#pragma once
+
+
+
+
+namespace app
+{
+	namespace actor
+	{
+
+		// 前方宣言
+		class ActorStateMachine;
+
+
+		class IState
+		{
+		private:
+			/** ステートマシン */
+			ActorStateMachine* m_stateMachine;
+
+
+		public:
+			IState();
+			virtual ~IState() = default;
+
+
+		public:
+			virtual void Enter() = 0;
+			virtual void Update() = 0;
+			virtual void Exit() = 0;
+
+
+		protected:
+			/** ステートマシンを取得する */
+			template<typename TStateMachine>
+			TStateMachine* GetOwner()
+			{
+				return dynamic_cast<TStateMachine*>(m_stateMachine);
+			}
+		};
+
+
+		class ActorStateMachine
+		{
+			// ステートのマップ
+			// キー: uint8_t
+			// 中身: std::unique_ptr<IState>
+			using StateMap = std::unordered_map<uint8_t, std::unique_ptr<IState>>;
+		private:
+			/** ステートマップ */
+			StateMap m_stateMap;
+			/** 現在のステート */
+			IState* m_currentState;
+			/** 次のステート */
+			IState* m_nextState;
+
+
+		public:
+			/*
+			 * ステートマシンを更新する
+			 * NOTE: ステートマシンの持ち主が毎フレーム呼び出すこと
+			 */
+			void Update();
+
+
+			/** ステートを変更させる */
+			void ChangeState();
+
+
+		protected:
+			/*
+			 * ステートの変更先を取得する
+			 */
+			virtual IState* GetChangeState() = 0;
+
+
+			/** ステートを取得する */
+			IState* FindState(const uint8_t stateID);
+
+
+			/** ステートを追加する */
+			template<typename TState>
+			void AddState(const uint8_t stateID)
+			{
+				auto it = m_stateMap.find(stateID);
+				if (it == m_stateMap.end())
+				{
+					delete it->second;
+					K2_ASSERT(false, "重複しています");
+				}
+				m_stateMap[stateID] = std::make_unique<TState>(this);
+			}
+
+
+		public:
+			ActorStateMachine();
+			virtual ~ActorStateMachine() = default;
+		};
+	}
+}
+
