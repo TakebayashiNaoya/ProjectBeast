f378fdfb8476674bde4391dbb3577fb48f4b44bc
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-02-02 21:00:51 +0900

    不要な処理を削除

--- Game/stdafx.h
+++ Game/stdafx.h
@@ -1,6 +1,6 @@
-#pragma once
+﻿#pragma once
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "k2EnginePreCompile.h"
-using namespace nsK2EngineLow;
-using namespace nsK2Engine;
+//#include "k2EnginePreCompile.h"
+//using namespace nsK2EngineLow;
+//using namespace nsK2Engine;
