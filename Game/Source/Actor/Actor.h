a2a46269f990e8c52e2dac047c71e29e24a3e012
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-02-02 20:51:09 +0900

    Actorクラス実装
    TransformとModelRenderをもっています。
    必要になれば追加していきます。

---
+++ Game/Source/Actor/Actor.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include "Source/Core/Transform.h"
+
+
+
+
+namespace app
+{
+	namespace actor
+	{
+		class Actor : public IGameObject
+		{
+		protected:
+			/** トランスフォーム */
+			core::Transform m_transform;
+			/** モデルレンダー */
+			ModelRender m_modelRender;
+
+
+		public:
+			/** トランスフォームを取得 */
+			inline const core::Transform& GetTransform() const { return m_transform; }
+
+
+			/** モデルレンダーを取得 */
+			inline ModelRender& GetModelRender() { return m_modelRender; }
+
+
+			/** 座標を設定 */
+			void SetPosition(const Vector3& position) { m_transform.m_position = position; }
+
+
+			/** 回転を設定 */
+			void SetRotation(const Quaternion& rotation) { m_transform.m_rotation = rotation; }
+
+
+			/** 拡縮を設定 */
+			void SetScale(const Vector3& scale) { m_transform.m_scale = scale; }
+
+
+		public:
+			Actor() = default;
+			virtual ~Actor() = default;
+
+
+		public:
+			virtual bool Start()override = 0;
+			virtual void Update()override = 0;
+			virtual void Render(RenderContext& rc)override = 0;
+		};
+	}
+}
+
