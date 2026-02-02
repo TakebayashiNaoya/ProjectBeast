fd2b28967df6b0d1f958b17528ef408f9b2fe0e4
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-02-02 17:34:55 +0900

    Transform実装
    m_position,m_rotation,m_scaleを持っています。
    必要になれば追加していきます。

---
+++ Game/Source/Core/Transform.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+
+
+
+namespace app
+{
+	namespace core
+	{
+		/*
+		 *@brief トランスフォームクラス
+		 */
+		class Transform
+		{
+		public:
+			/** 座標 */
+			Vector3 m_position;
+
+			/** 回転 */
+			Quaternion m_rotation;
+
+			/** スケール */
+			Vector3 m_scale;
+
+
+		public:
+			Transform();
+			~Transform() = default;
+		};
+	}
+}
+
