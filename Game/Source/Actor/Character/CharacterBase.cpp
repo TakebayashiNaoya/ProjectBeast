02bd3d099a154f2eb868043967608c9c8c0550bd
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-02-03 09:30:58 +0900

    CharacterBaseクラス実装
    各キャラクターはこのクラスを継承してください。
    Init()関数で、m_modelRenderの初期化を行ってください。
    また、各クラスの.cppファイルでAnimationData、ModelDataの定義を行ってください。
    これらはInit()の引数に使用します。

---
+++ Game/Source/Actor/Character/CharacterBase.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include "CharacterBase.h"
+#include "CharacterStatus.h"
+
+
+
+
+namespace app
+{
+	namespace actor
+	{
+		void CharacterBase::Init(const ModelData* data)
+		{
+			// アニメーションクリップを作成
+			m_animationClips = new AnimationClip[data->clipNum];
+
+			for (int i = 0; i < data->clipNum; ++i)
+			{
+				m_animationClips[i].Load(data->animationData->fileName);
+				m_animationClips[i].SetLoopFlag(data->animationData->isLoop);
+			}
+
+			// モデルをセットアップ
+			m_modelRender.Init(data->fileName, m_animationClips, data->clipNum, data->upAxis);
+			// トランスフォームを設定
+			m_modelRender.SetTRS(m_transform.m_position, m_transform.m_rotation, m_transform.m_scale);
+			// モデルレンダーを更新
+			m_modelRender.Update();
+
+			const auto* status = GetStatus<CharacterStatus>();
+
+			// キャラクターコントローラーを初期化
+			m_characterController.Init(status->GetRadius(), status->GetHeight(), m_transform.m_position);
+		}
+	}
+}
---
+++ Game/Source/Actor/Character/CharacterBase.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+#include "Source/Actor/Actor.h"
+
+
+
+
+namespace app
+{
+	namespace actor
+	{
+
+		struct AnimationData
+		{
+			/** ファイルネーム */
+			const char* fileName;
+			/** ループフラグ */
+			const bool isLoop;
+		};
+
+
+		struct ModelData
+		{
+			/** ファイルネーム */
+			const char* fileName;
+			/** アニメーションデータ */
+			const AnimationData* animationData;
+			/** モデルの上方向 */
+			const EnModelUpAxis upAxis;
+			/** アニメーションクリップの数 */
+			const uint8_t clipNum;
+		};
+
+
+		class CharacterBase : public Actor
+		{
+		protected:
+			/** アニメーションクリップ */
+			AnimationClip* m_animationClips;
+			/** キャラクターコントローラー */
+			CharacterController m_characterController;
+
+
+		public:
+			/** キャラクターコントローラーを取得 */
+			inline CharacterController* GetCharacterController() { return &m_characterController; }
+
+
+		protected:
+			/** 初期化 */
+			//template<typename TStatus>
+			void Init(const ModelData* data);
+		};
+	}
+}
+
