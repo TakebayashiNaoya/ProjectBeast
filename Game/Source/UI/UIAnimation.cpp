91d49e3128dde842d4bc39e7fe3f1c78770553c5
kutsunaharuki <ca01254007@st.kawahara.ac.jp>
2026-02-04 19:09:03 +0900

    基本的なUIAnimationBaseの追加 (#22)
    Co-authored-by: kutsunaharuki <ca01254@st.kawahara.ac.jp>
    Co-authored-by: 竹林 尚哉 <ca01254013@st.kawahara.ac.jp>

---
+++ Game/Source/UI/UIAnimation.cpp
@@ -0,0 +1,19 @@
+﻿/**
+ * @file UIAnimation.cpp
+ * @brief UIAnimationのクラス
+ * @author 忽那
+ */
+#include "stdafx.h"
+#include "UIAnimation.h"
+
+
+namespace app
+{
+	namespace ui
+	{
+		UIAnimationBase::UIAnimationBase()
+			: m_ui(nullptr)
+		{
+		}
+	}
+}
---
+++ Game/Source/UI/UIAnimation.h
@@ -0,0 +1,46 @@
+﻿/**
+ * @file UIAnimation.h
+ * @brief UIAnimationの機能群
+ * @author 忽那
+ */
+#pragma once
+
+
+namespace app
+{
+	namespace ui
+	{
+		/** 前方宣言 */
+		class UIBase;
+
+
+		/**
+		 * @brief UIAnimationの機能
+		 */
+		class UIAnimationBase
+		{
+		public:
+			UIAnimationBase();
+			~UIAnimationBase() = default;
+
+
+			/** 純粋仮想関数 */
+			virtual void Update() = 0;
+			virtual void PlayAnimation() = 0;
+			virtual bool IsPlayAnimation() = 0;
+
+
+		public:
+			/**
+			 * @brief UIの設定
+			 * @param ui UIBaseのポインタ
+			 */
+			void SetUI(UIBase* ui) { m_ui = ui; }
+
+
+		protected:
+			/** UIBaseのポインタ */
+			UIBase* m_ui;
+		};
+	}
+}
