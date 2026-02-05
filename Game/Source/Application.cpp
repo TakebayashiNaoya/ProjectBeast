ad02e8c591e49c629f6e61d61e46c65997129086
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-02-05 14:29:08 +0900

    Applicationの追加

---
+++ Game/Source/Application.cpp
@@ -0,0 +1,25 @@
+﻿#include "stdafx.h"
+#include "Application.h"
+
+
+namespace app
+{
+	Application::Application()
+	{
+	}
+
+
+	Application::~Application()
+	{
+	}
+
+
+	void Application::Update()
+	{
+	}
+
+
+	void Application::Render(RenderContext& rc)
+	{
+	}
+}
