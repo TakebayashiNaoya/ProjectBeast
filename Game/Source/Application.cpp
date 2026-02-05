feff52345b9de365e8b9aaa358672993982461c2
tachiyamakotaro <ca01254014@st.kawahara.ac.jp>
2026-02-05 15:23:20 +0900

    DebugSceneの追加 (#29)
    Co-authored-by: Tachiyama Kotarou <lishanhutailang3@gmail.com>

--- Game/Source/Application.cpp
+++ Game/Source/Application.cpp
@@ -1,25 +1,30 @@
 ﻿#include "stdafx.h"
 #include "Application.h"
+#include "Source/Scene/SceneManager.h"
 
 
 namespace app
 {
 	Application::Application()
 	{
+		SceneManager::CreateInstance();
 	}
 
 
 	Application::~Application()
 	{
+		SceneManager::DestroyInstance();
 	}
 
 
 	void Application::Update()
 	{
+		SceneManager::GetInstance()->Update();
 	}
 
 
 	void Application::Render(RenderContext& rc)
 	{
+		SceneManager::GetInstance()->Render(rc);
 	}
 }
---
+++ Game/Source/Scene/DebugScene.cpp
@@ -0,0 +1,37 @@
+﻿#include "stdafx.h"
+#include "DebugScene.h"
+
+
+namespace app
+{
+	DebugScene::DebugScene()
+	{
+	}
+
+
+	DebugScene::~DebugScene()
+	{
+	}
+
+
+	bool DebugScene::Start()
+	{
+		return true;
+	}
+
+
+	void DebugScene::Update()
+	{
+	}
+
+
+	void DebugScene::Render(RenderContext& rc)
+	{
+	}
+
+
+	bool DebugScene::RequesutScene(uint32_t& id)
+	{
+		return false;
+	}
+}
