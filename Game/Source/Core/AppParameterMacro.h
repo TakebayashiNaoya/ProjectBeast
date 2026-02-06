6073428d3af89a427df1f8d7ad4f335631c00ac3
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-02-06 21:32:00 +0900

    マクロを定義
    ParameterManagerに必要なマクロをファイル分けし、定義しました。

---
+++ Game/Source/Core/AppParameterMacro.h
@@ -0,0 +1,44 @@
+﻿/**
+ * @file AppParameterMacro.h
+ * @brief パラメーターに必要なマクロ定義
+ * @author 藤谷
+ */
+#pragma once
+#include "Source/Util/CRC32.h"
+
+
+namespace app
+{
+	namespace core
+	{
+
+	}
+}
+
+
+/**
+ * @note デバッグビルド時のみ有効化
+ */
+#ifdef K2_DEBUG
+#define APP_PARAM_HOT_RELOAD
+#endif
+
+
+ /**
+ * @note ホットリロード対応版
+ */
+#ifdef APP_PARAM_HOT_RELOAD
+
+#define appParameter(name)\
+public:\
+static constexpr uint32_t ID() {return Hash32(#name);}\
+std::function<void(const nlohmann::json& j, name& p)> load;
+
+#else
+
+#define appParameter(name)\
+public:\
+static constexpr uint32_t ID() {return Hash32(#name);}
+
+#endif
+
