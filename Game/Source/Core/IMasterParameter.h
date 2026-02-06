9de91eb7a1c3c16c58f815780d1889e26bfaa0c8
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-02-06 21:33:09 +0900

    IMasterParameterをファイル分け
    パラメーター全体の基底クラスをファイル分けして定義しまし。

---
+++ Game/Source/Core/IMasterParameter.h
@@ -0,0 +1,25 @@
+﻿/**
+ * @file IMasterParameter.h
+ * @brief パラメーター基底クラス
+ * @author 藤谷
+ */
+#pragma once
+#include "AppParameterMacro.h"
+#include "json/json.hpp"
+
+
+namespace app
+{
+	namespace core
+	{
+		/** 基底クラス。必ず継承すること！ */
+		struct IMasterParameter
+		{
+#ifdef APP_PARAM_HOT_RELOAD
+			virtual void Load(const nlohmann::json& j) {};
+			std::string m_path;         // パラメーターのファイルパス（ホットリロード用）
+			time_t m_lastWriteTime;     // 最終更新時刻
+#endif // APP_PARAM_HOT_RELOAD
+		};
+	}
+}
