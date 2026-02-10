2918fd846d5e78541d94f4792dfef6d09e34e270
kutsunaharuki <ca01254@st.kawahara.ac.jp>
2026-02-10 23:56:14 +0900

    UIAnimationの拡張機能を追加
    Floatアニメーションの追加
    Vector2,Vector3,Vector4アニメーションの追加
    座標,拡縮,回転,差分アニメーションの追加

--- Game/Source/UI/UIAnimation.cpp
+++ Game/Source/UI/UIAnimation.cpp
@@ -5,15 +5,79 @@
  */
 #include "stdafx.h"
 #include "UIAnimation.h"
+#include "Source/UI/UIParts.h"
 
 
 namespace app
 {
 	namespace ui
 	{
-		UIAnimationBase::UIAnimationBase()
-			: m_ui(nullptr)
+		UIColorAnimation::UIColorAnimation()
 		{
+			SetFunc([&](Vector4 v)
+				{
+					m_ui->m_color = v;
+				});
+		}
+
+
+
+
+
+		/******************************************/
+
+
+		UIScaleAnimation::UIScaleAnimation()
+		{
+			SetFunc([&](Vector3 s)
+				{
+					m_ui->m_transform.m_localTransform.m_scale = s;
+				});
+		}
+
+
+
+
+
+		/******************************************/
+
+
+		UITranslateAnimation::UITranslateAnimation()
+		{
+			SetFunc([&](Vector3 s)
+				{
+					m_ui->m_transform.m_localTransform.m_position = s;
+				});
+		}
+
+
+
+
+
+		/******************************************/
+
+
+		UITranslateOffsetAnimation::UITranslateOffsetAnimation()
+		{
+			SetFunc([&](Vector3 offset)
+				{
+					m_ui->m_transform.m_localTransform.m_position.Add(offset);
+				});
+		}
+
+
+
+
+
+		/******************************************/
+
+
+		UIRotationAnimation::UIRotationAnimation()
+		{
+			SetFunc([&](float s)
+				{
+					m_ui->m_transform.m_localTransform.m_rotation.SetRotationDegZ(s);
+				});
 		}
 	}
 }
