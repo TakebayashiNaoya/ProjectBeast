2520f92dae3e022cedb79e3748eadccc271c9de3
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-02-10 19:12:15 +0900

    ディレクションライトの構造体の追加

--- BeastEngine/graphics/Light/SceneLight.h
+++ BeastEngine/graphics/Light/SceneLight.h
@@ -1,5 +1,21 @@
-﻿#pragma once
-class SceneLight
+﻿/**
+ * @file SceneLight.h
+ * @brief シーンライトに関する構造体をまとめたヘッダファイル
+ * @author 竹林尚哉
+ */
+#pragma once
+
+
+namespace nsBeastEngine
 {
-};
+	/**
+	 * @brief ディレクションライト
+	 */
+	struct DirectionalLight
+	{
+		Vector3 direction;  /** ライトの方向 */
+		int castShadow;     /** 影をキャストするかどうか */
+		Vector4 color;      /** ライトのカラー */
+	};
+}
 
