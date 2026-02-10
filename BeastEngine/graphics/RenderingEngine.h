f6792939fd45ec112ed56527667d0617078b3eba
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-02-10 19:11:56 +0900

    レイトレ用のライトデータを追加

--- BeastEngine/graphics/RenderingEngine.h
+++ BeastEngine/graphics/RenderingEngine.h
@@ -5,6 +5,7 @@
  */
 #pragma once
 #include "MyRenderer.h"
+#include "Graphics/Light/SceneLight.h"
 
 
 namespace nsBeastEngine
@@ -17,16 +18,33 @@ namespace nsBeastEngine
 
 
 	public:
-
 		/**
 		 * @brief レンダリングパイプラインの初期化
 		 * @param isSoftShadow	ソフトシャドウを使用するかどうか
 		 */
 		void Init(bool isSoftShadow);
 
+		/**
+		 * @brief レイトレ用のライトデータ
+		 */
+		struct RaytracingLightData
+		{
+			DirectionalLight m_directionalLight;  /** ディレクショナルライト */
+			Vector3 m_ambientLight;               /** 環境光（IBLテクスチャが指定されていない場合に利用される） */
+			float m_iblIntencity;                 /** IBL強度 */
+			int m_enableIBLTexture;               /** IBLテクスチャが指定されている */
+		};
+		/**
+		 * @brief レイトレ用のライトデータを取得
+		 * @return レイトレ用のライトデータ
+		 */
+		RaytracingLightData& GetRaytracingLightData()
+		{
+			return m_raytracingLightData;
+		}
+
 
 	private:
-
 		/**
 		 * @brief メインレンダリングターゲットの初期化
 		 */
@@ -36,5 +54,7 @@ namespace nsBeastEngine
 	private:
 		/**	メインレンダリングターゲット */
 		RenderTarget m_mainRenderTarget;
+		/** レイトレ用のライトデータ */
+		RaytracingLightData m_raytracingLightData;
 	};
 }
