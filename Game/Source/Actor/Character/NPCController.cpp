3d3bccfc292e9f341d11ccbc123559f4a4a135fb
tachiyamakotaro <ca01254014@st.kawahara.ac.jp>
2026-02-17 15:45:27 +0900

    仮のNPCControllerを作りました。 (#49)
    Co-authored-by: Tachiyama Kotarou <lishanhutailang3@gmail.com>

---
+++ Game/Source/Actor/Character/NPCController.cpp
@@ -0,0 +1,95 @@
+﻿/**
+ * @file NPCController.cpp
+ * @brief NPCのコントローラー
+ * @author 立山
+ */
+#include "stdafx.h"
+#include "NPCController.h"
+
+
+namespace app
+{
+	NPCController::NPCController()
+	{
+	}
+
+
+	NPCController::~NPCController()
+	{
+	}
+
+
+	void NPCController::Start()
+	{
+		StartWrapper();
+	}
+
+
+	void NPCController::Update()
+	{
+		UpdateWrapper();
+		auto* currentState = FindAIState(m_currentState);
+		if (currentState == nullptr) {
+			K2_ASSERT(false, "対象の処理が見つかりません\n");
+			return;
+		}
+
+		/** 初回起動時のEnter処理 */
+		if (!m_isInitialized) {
+			currentState->enter(this);
+			m_isInitialized = true;
+		}
+
+		/** 遷移判定 */
+		const int nextState = currentState->check(this);
+		if (nextState != -1 && nextState != m_currentState) {
+			ChangeState((EnAIStateID)nextState);
+			currentState = FindAIState(m_currentState);
+		}
+
+		/** 現在のステートのアップデート */
+		currentState->update(this);
+
+	}
+
+
+	void NPCController::Render(RenderContext& renderContext)
+	{
+		//RenderWrapper(renderContext);
+	}
+
+
+	void NPCController::ChangeState(EnAIStateID nextState)
+	{
+		/** 指定したnextStateがおかしい */
+		if (nextState < enAIState_Invalid || nextState >= enAIState_Num) {
+			return;
+		}
+
+		auto* currentState = FindAIState(m_currentState);
+		/** 現在のステートのExit処理 */
+		currentState->exit(this);
+		/** ステートの更新 */
+		m_currentState = nextState;
+		/** 新しいステートのEnterを呼ぶ */
+		currentState = FindAIState(m_currentState);
+		currentState->enter(this);
+	}
+
+
+	/** 各ステートの実装 */
+	void NPCController::Initialize()
+	{
+		/** 待機 */
+		RegisterState(enAIState_Idle, EnterIdle, UpdateIdle, ExitIdle, CheckIdle);
+		/** 移動 */
+		RegisterState(enAIState_Move, EnterMove, UpdateMove, ExitMove, CheckMove);
+		/** ジャンプ */
+		RegisterState(enAIState_Jump, EnterJump, UpdateJump, ExitJump, CheckJump);
+		/** 泳ぐ */
+		RegisterState(enAIState_Swim, EnterSwim, UpdateSwim, ExitSwim, CheckSwim);
+	}
+
+
+	/** 各ステートの処理はこの下に書いていく */
+}
---
+++ Game/Source/Actor/Character/NPCController.h
@@ -0,0 +1,158 @@
+﻿/**
+ * @file NPCController.h
+ * @brief NPCのコントローラー
+ * @author 立山
+ */
+#pragma once
+#include "IObject.h"
+
+
+namespace app {
+
+	class NPCController :public IObject
+	{
+
+	public:
+		NPCController();
+		~NPCController();
+		void Start() override;
+		void Update() override;
+		void Render(RenderContext& renderContext) override;
+
+
+	public:
+		static void Initialize();
+
+
+	private:
+		/**
+		 * 関数ポインタ
+		 */
+		using EnterFunc = void (*)(NPCController*);
+		using UpdateFunc = void (*)(NPCController*);
+		using ExitFunc = void (*)(NPCController*);
+		using CheckFunc = int (*)(NPCController*);
+
+	private:
+		/** AI思考処理 */
+		struct AIState
+		{
+			EnterFunc enter;
+			UpdateFunc update;
+			ExitFunc exit;
+			CheckFunc check;
+		};
+
+
+	private:
+		/**
+		 * @enum EnAIStateID
+		 * 思考パターンのID
+		*/
+		enum EnAIStateID
+		{
+			enAIState_Idle,
+			enAIState_Move,
+			enAIState_Jump,
+			enAIState_Swim,
+
+			enAIState_Num,
+			enAIState_Invalid = -1
+		};
+
+
+	private:
+		void ChangeState(EnAIStateID nextState);
+
+
+	private:
+		static std::map<EnAIStateID, AIState> m_stateMap;
+
+
+	private:
+		/**
+		 * @brief AIStateを登録する関数
+		 * @param id ステートのID
+		 * @param enter Enter関数
+		 * @param update Update関数
+		 * @param exit Exit関数
+		 * @param check Check関数
+		 */
+		static void RegisterState(const EnAIStateID id, EnterFunc enter, UpdateFunc update, ExitFunc exit, CheckFunc check)
+		{
+			AIState state;
+			/** 引数が nullptr ならDoNothingを入れる */
+			state.enter = (enter != nullptr) ? enter : DoNothing;
+			state.update = (update != nullptr) ? update : DoNothing;
+			state.exit = (exit != nullptr) ? exit : DoNothing;
+			state.check = (check != nullptr) ? check : CheckNothing;
+			// mapに登録
+			m_stateMap.emplace(id, state);
+		}
+
+
+		/** AIStateを探す */
+		AIState* FindAIState(const EnAIStateID id)
+		{
+			auto it = m_stateMap.find(id);
+			if (it != m_stateMap.end()) {
+				return &it->second;
+			}
+			return nullptr;
+		}
+
+
+		/**
+		 * 何もしない関数
+		 */
+		static void  DoNothing(NPCController*) {};
+		/** 遷移なし */
+		static int CheckNothing(NPCController*) { return -1; }
+
+
+	private:
+		/**
+		 * 待機
+		 */
+		static void EnterIdle(NPCController* npc);
+		static void UpdateIdle(NPCController* npc);
+		static void ExitIdle(NPCController* npc);
+		static int CheckIdle(NPCController* npc);
+
+
+		/**
+		 * 移動
+		 */
+		static void EnterMove(NPCController* npc);
+		static void UpdateMove(NPCController* npc);
+		static void ExitMove(NPCController* npc);
+		static int CheckMove(NPCController* npc);
+
+
+		/**
+		 * ジャンプ
+		 */
+		static void EnterJump(NPCController* npc);
+		static void UpdateJump(NPCController* npc);
+		static void ExitJump(NPCController* npc);
+		static int CheckJump(NPCController* npc);
+
+
+		/**
+		 * 泳ぐ
+		 */
+		static void EnterSwim(NPCController* npc);
+		static void UpdateSwim(NPCController* npc);
+		static void ExitSwim(NPCController* npc);
+		static int CheckSwim(NPCController* npc);
+
+
+	private:
+
+		/** 現在の状態 */
+		EnAIStateID m_currentState = enAIState_Idle;
+		/** 初期化処理をしたか */
+		bool m_isInitialized = false;
+
+	};
+}
