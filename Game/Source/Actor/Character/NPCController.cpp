7b6a77050fc05a056e041d601262393971e02078
tachiyamakotaro <ca01254014@st.kawahara.ac.jp>
2026-02-17 18:02:27 +0900

    NPCcontrollerのエラーを修正しました。 (#50)
    Co-authored-by: Tachiyama Kotarou <lishanhutailang3@gmail.com>

--- Game/Source/Actor/Character/NPCController.cpp
+++ Game/Source/Actor/Character/NPCController.cpp
@@ -9,6 +9,10 @@
 
 namespace app
 {
+	// Static変数の初期化
+	std::map<NPCController::EnAIStateID, NPCController::AIState> NPCController::m_stateMap;
+
+
 	NPCController::NPCController()
 	{
 	}
@@ -92,4 +96,80 @@ namespace app
 
 
 	/** 各ステートの処理はこの下に書いていく */
+
+	/** 待機 */
+	void NPCController::EnterIdle(NPCController* npc)
+	{
+
+	}
+
+	void NPCController::UpdateIdle(NPCController* npc)
+	{
+	}
+
+	void NPCController::ExitIdle(NPCController* npc)
+	{
+	}
+
+	int NPCController::CheckIdle(NPCController* npc)
+	{
+		return enAIState_Invalid;
+	}
+
+
+	/** 移動 */
+	void NPCController::EnterMove(NPCController* npc)
+	{
+	}
+
+	void NPCController::UpdateMove(NPCController* npc)
+	{
+	}
+
+	void NPCController::ExitMove(NPCController* npc)
+	{
+	}
+
+	int NPCController::CheckMove(NPCController* npc)
+	{
+		return enAIState_Invalid;
+	}
+
+
+	/** ジャンプ */
+	void NPCController::EnterJump(NPCController* npc)
+	{
+	}
+
+	void NPCController::UpdateJump(NPCController* npc)
+	{
+	}
+
+	void NPCController::ExitJump(NPCController* npc)
+	{
+	}
+
+	int NPCController::CheckJump(NPCController* npc)
+	{
+		return enAIState_Invalid;
+	}
+
+
+	/** 泳ぐ */
+	void NPCController::EnterSwim(NPCController* npc)
+	{
+	}
+
+	void NPCController::UpdateSwim(NPCController* npc)
+	{
+	}
+
+	void NPCController::ExitSwim(NPCController* npc)
+	{
+	}
+
+	int NPCController::CheckSwim(NPCController* npc)
+	{
+		return enAIState_Invalid;
+	}
 }
