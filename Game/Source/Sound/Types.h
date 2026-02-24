ef233ed0de777fbcb21eb8505730684aecdf9a61
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-02-24 15:13:37 +0900

    Types.hの追加

---
+++ Game/Source/Sound/Types.h
@@ -0,0 +1,17 @@
+﻿/**
+ * @file Types.h
+ * @brief サウンド用の定数やファイルパスなどを定義するクラス
+ * @author 立山
+ */
+#pragma once
+
+
+namespace app
+{
+	enum enSoundKind
+	{
+		enSoundKind_SE = 0,
+		enSoundKind_SE_Max,
+		enSoundKind_BGM = enSoundKind_SE_Max,
+	};
+}
