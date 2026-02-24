0f771de872a2771cb0f0554deab4cb1066f82aa9
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-02-24 16:37:44 +0900

    Typesの実装とコメントの追加、SE,BGM,Voiceのデータの追加

--- Game/Source/Sound/Types.h
+++ Game/Source/Sound/Types.h
@@ -12,9 +12,25 @@ namespace app
 	enum enSoundKind
 	{
 		enSoundKind_SE = 0,
+
+		/** 下にSEの設定 */
+		enSoundKind_Damage = enSoundKind_SE,
+
+		/** SEの設定はここまで */
 		enSoundKind_SE_Max,
+
 		enSoundKind_BGM = enSoundKind_SE_Max,
+		/** 下にBGMの設定 */
+		enSoundKind_Game = enSoundKind_BGM,
+
+		/** BGMの設定はここまで */
+
 		enSoundKind_Voice,
+		/** 下にVoiceの設定 */
+		enSoundKind_Penguin = enSoundKind_Voice,
+
+		/** Voiceの設定はここまで */
+
 		enSoundKind_Max,
 		enSoundKind_None = enSoundKind_Max,
 	};
@@ -30,9 +46,13 @@ namespace app
 
 
 	/** 情報を保持 */
-	//static SoundInformation soundInformation[enSoundKind_Max] =
-	//{
-	//	//SE
-
-	//};
+	static SoundInformation soundInformation[enSoundKind_Max] =
+	{
+		//SE
+		SoundInformation("Assets/sound/SE/damage.wav"),
+		//BGM
+		SoundInformation("Assets/sound/BGM/InGame.wav"),
+		//Voice
+		SoundInformation("Assets/sound/Voice/penguin.wav")
+	};
 }
