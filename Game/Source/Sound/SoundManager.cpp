b7d0145fc565647cb80355932b025eee7d22128b
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-02-24 11:16:04 +0900

    サウンドマネージャークラスの追加

---
+++ Game/Source/Sound/SoundManager.cpp
@@ -0,0 +1,6 @@
+﻿/**
+ * @file SoundManager.h
+ * @brief サウンドの管理をするクラス
+ * @author 立山
+ */
+#include "stdafx.h"
---
+++ Game/Source/Sound/SoundManager.h
@@ -0,0 +1,7 @@
+﻿/**
+ * @file SoundManager.h
+ * @brief サウンドの管理をするクラス
+ * @author 立山
+ */
+#pragma once
+#include "SoundManager.h"
