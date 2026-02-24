4273e733c56cb115bbd667ea7d9341093edb4df2
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-02-24 15:03:12 +0900

    SoundManagerクラスの実装

--- Game/Source/Sound/SoundManager.cpp
+++ Game/Source/Sound/SoundManager.cpp
@@ -5,3 +5,88 @@
  */
 #include "stdafx.h"
 #include "SoundManager.h"
+
+
+namespace app
+{
+	SoundManager* SoundManager::m_instance = nullptr;
+
+
+	SoundManager::SoundManager()
+	{
+		m_seList.clear();
+	}
+
+
+	SoundManager::~SoundManager()
+	{
+	}
+
+
+	void SoundManager::Update()
+	{
+		/** SEリストから再生していないものがあれば削除する */
+		std::vector<SoundHandle> eraseList;
+		for (auto& it : m_seList) {
+			const auto key = it.first;
+			auto* se = it.second;
+			/** 再生が終わっているなら削除 */
+			if (!se->IsPlaying()) {
+				eraseList.push_back(key);
+			}
+		}
+		for (const auto& key : eraseList) {
+			m_seList.erase(key);
+		}
+	}
+
+
+	void SoundManager::PlayBGM(const int kind)
+	{
+		if (m_bgm == nullptr) {
+			m_bgm = new SoundSource;
+		}
+		else {
+			m_bgm->Stop();
+		}
+		/** 初期化 */
+		m_bgm->Init(kind);
+		m_bgm->Play(true);
+	}
+
+
+	void SoundManager::StopBGM()
+	{
+		if (m_bgm == nullptr) {
+			return;
+		}
+		m_bgm->Stop();
+	}
+
+
+	SoundHandle SoundManager::PlaySE(const int kind, const bool isLoop, const bool is3D)
+	{
+		/** ハンドルが最大数になったら使えない */
+		if (m_soundHandleCount == INVALID_SOUND_HANDLE) {
+			K2_ASSERT(false, "サウンドの再生が多いです。\n");
+			return INVALID_SOUND_HANDLE;
+		}
+		auto* se = new SoundSource;
+		se->Init(kind, is3D);
+		se->Play(isLoop);
+
+		m_seList.emplace(m_soundHandleCount++, se);
+
+		return m_soundHandleCount;
+	}
+
+
+	void SoundManager::StopSE(const SoundHandle handle)
+	{
+		auto* se = FindSE(handle);
+		if (se == nullptr) {
+			return;
+		}
+		se->Stop();
+	}
+}
--- Game/Source/Sound/SoundManager.h
+++ Game/Source/Sound/SoundManager.h
@@ -4,3 +4,108 @@
  * @author 立山
  */
 #pragma once
+
+
+namespace app
+{
+	/** サウンド用のハンドル名 */
+	using SoundHandle = uint32_t;
+	/** ハンドル無効値 */
+	static constexpr SoundHandle INVALID_SOUND_HANDLE = 0xffffffff;
+
+
+	/**
+	 * サウンドを管理するクラス
+	 */
+	class SoundManager
+	{
+	public:
+		/**
+		 * 更新処理
+		 * @brief 不要なインスタンスを削除したりする
+		 * @detail main.cppで呼び出す
+		 */
+		void Update();
+
+
+	public:
+		/** BGMの再生 */
+		void PlayBGM(const int kind);
+		/** BGMの停止 */
+		void StopBGM();
+
+
+		/** SEの再生 */
+		SoundHandle PlaySE(const int kind, const bool isLoop = false, const bool is3D = false);
+		/** SEの停止 */
+		void StopSE(const SoundHandle handle);
+
+
+		SoundSource* FindSE(const SoundHandle handle)
+		{
+			auto it = m_seList.find(handle);
+			if (it != m_seList.end()) {
+				return it->second;
+			}
+			K2_ASSERT(false, "削除済みか追加されていないSEにアクセスしようとしています。\n");
+			return nullptr;
+		}
+
+
+	public:
+		/**
+		 * インスタンスを作成
+		 * @detail main.cppで呼び出す
+		 */
+		static void CreateInstance()
+		{
+			if (m_instance == nullptr)
+			{
+				m_instance = new SoundManager();
+			}
+		}
+
+
+		/**
+		 * インスタンスを取得
+		 */
+		static SoundManager& Get()
+		{
+			return *m_instance;
+		}
+
+
+		/**
+		 * インスタンスを削除
+		 */
+		static void DestroyInstance()
+		{
+			if (m_instance != nullptr)
+			{
+				delete m_instance;
+				m_instance = nullptr;
+			}
+		}
+
+
+	private:
+		/** BGM用のサウンドソースインスタンスを保持する */
+		SoundSource* m_bgm = nullptr;
+		/** SE用のサウンドソースインスタンスを保持する */
+		std::map<SoundHandle, SoundSource*> m_seList;
+		/**
+		 * マップで参照するようにハンドル数を保持
+		 */
+		SoundHandle m_soundHandleCount = 0;
+
+
+	private:
+		SoundManager();
+		~SoundManager();
+
+
+	private:
+		/** シングルトンインスタンス */
+		static SoundManager* m_instance;
+	};
+}
