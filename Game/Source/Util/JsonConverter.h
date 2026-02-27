dd20dec9683456c5cfeecbd180909f8e181a016d
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-02-27 11:16:15 +0900

    jsonから変換する関数を実装
    必要なものが増え次第追加していきます。

---
+++ Game/Source/Util/JsonConverter.h
@@ -0,0 +1,52 @@
+﻿/**
+ * @file JsonLoadingInfomation.h
+ * @brief jsonファイルの読み込みに使用する
+ * @author 藤谷
+ */
+#pragma once
+#include "Json/json.hpp"
+
+
+namespace app
+{
+	namespace util
+	{
+		/**
+		 * @brief jsonファイルの読み込みに使用する関数群
+		 */
+		class JsonConverter
+		{
+		public:
+			/**
+			 * @brief jsonファイルを読み込めたかどうか
+			 * @param json 読み込むjsonファイル
+			 * @return 読み込めたかどうか
+			 */
+			static bool IsLoadJsonFile(nlohmann::json& json, const std::string& filePath);
+			/**
+			 * @brief jsonからfloatを読み込む
+			 * @param json 読み込むjsonファイル
+			 * @return 読み込んだfloat
+			 */
+			static float ToFloat(const nlohmann::json& json);
+			/**
+			 * @brief jsonからstringを読み込む
+			 * @param json 読み込むjsonファイル
+			 * @return 読み込んだstring
+			 */
+			static std::string ToString(const nlohmann::json& json);
+			/**
+			 * @brief jsonからVector3を読み込む
+			 * @param json 読み込むjsonファイル
+			 * @return 読み込んだVector3
+			 */
+			static Vector3 ToVector3(const nlohmann::json& json);
+
+
+		private:
+			// インスタンス化を禁止
+			JsonConverter() = delete;
+			~JsonConverter() = delete;
+		};
+	}
+}
