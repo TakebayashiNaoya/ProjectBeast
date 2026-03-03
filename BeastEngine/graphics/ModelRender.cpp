a227a2b5cab2f88d586606ca3be3089f4129ac80
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-03-03 14:35:06 +0900

    Add: モデルレンダーの実装

--- BeastEngine/graphics/ModelRender.cpp
+++ BeastEngine/graphics/ModelRender.cpp
@@ -1,258 +1,126 @@
-﻿#include "BeastEnginePreCompile.h"
+﻿/**
+ * @file ModelRender.cpp
+ * @brief モデルレンダーの実装
+ * @author 竹林尚哉
+ */
+#include "BeastEnginePreCompile.h"
 #include "ModelRender.h"
 
 
 namespace nsBeastEngine
 {
-	ModelRender::ModelRender()
-		: m_animationClips(nullptr)
-		, m_numAnimationClips(0)
-		, m_isEnableInstancingDraw(false)
-		, m_maxInstance(1)
-	{
-	}
-
-
-	ModelRender::~ModelRender()
-	{
-	}
-
-
 	void ModelRender::Init(
 		const char* filePath,
-		AnimationClip* animationClips,
+		AnimationClip* animationeClips,
 		int numAnimationClips,
-		EnModelUpAxis enModelUpAxis,
-		bool isShadowReciever,
-		int maxInstance,
-		bool isFrontCullingOnDrawShadowMap)
+		bool islighting,
+		EnModelUpAxis enModelUpAxiz)
 	{
-		// インスタンシング描画用のデータを初期化。
-		// NOTE: 今は必要ないのでコメントアウト。
-		//InitInstancingDraw(maxInstance);
-
-		// スケルトンを初期化。
+		/** スケルトンの初期化 */
 		InitSkeleton(filePath);
+		/** アニメーションの初期化 */
+		InitAnimation(animationeClips, numAnimationClips, enModelUpAxiz);
 
-		// アニメーションを初期化。
-		InitAnimation(animationClips, numAnimationClips, enModelUpAxis);
+		ModelInitData modelInitData;
+		/** tkmファイルのファイルパスの指定 */
+		modelInitData.m_tkmFilePath = filePath;
+		/** シェーダーのファイルパスの指定 */
+		if (islighting) {
+			modelInitData.m_fxFilePath = "Assets/shader/model.fx";
+		}
+		else {
+			modelInitData.m_fxFilePath = "Assets/shader/lightOffModel.fx";
+		}
+		/** シェーダーのエントリーポイントの設定 */
+		SetupShaderEntryPointFunc(modelInitData);
+		/** アニメーションがある場合はスケルトンを渡す */
+		if (animationeClips != nullptr) {
+			modelInitData.m_skeleton = &m_skeleton;
+		}
 
-		// アニメーション済み頂点バッファの計算処理を初期化。
-		InitComputeAnimatoinVertexBuffer(filePath, enModelUpAxis);
+		/** シャドウマップテクスチャ */
+		modelInitData.m_expandShaderResoruceView[0] = &g_renderingEngine->GetShadowMap();
 
-		// GBuffer描画用のモデルを初期化。
-		//InitModelOnRenderGBuffer(*g_renderingEngine, filePath, enModelUpAxis, isShadowReciever);
+		/** シーンライト */
+		modelInitData.m_expandConstantBuffer = g_sceneLight->GetLight();
+		modelInitData.m_expandConstantBufferSize = sizeof(Light);
+		m_model.Init(modelInitData);
 
-		// ZPrepass描画用のモデルを初期化。
-		//InitModelOnZprepass(*g_renderingEngine, filePath, enModelUpAxis);
+		/** シャドウマップテクスチャ */
+		modelInitData.m_expandShaderResoruceView[0] = nullptr;
 
-		// シャドウマップ描画用のモデルを初期化。
-		//InitModelOnShadowMap(*g_renderingEngine, filePath, enModelUpAxis, isFrontCullingOnDrawShadowMap);
+		modelInitData.m_fxFilePath = "Assets/shader/DrawShadowMap.fx";
+		modelInitData.m_colorBufferFormat[0] = DXGI_FORMAT_R32_FLOAT;
+		modelInitData.m_expandConstantBuffer = &g_sceneLight->GetLVP();
+		m_shadowModels.Init(modelInitData);
+	}
 
-		//// 幾何学データを初期化。
-		//InitGeometryDatas(maxInstance);
 
-		//// 各種ワールド行列を更新する。
-		//UpdateWorldMatrixInModes();
-
-		//if (m_isRaytracingWorld) {
-		//	// レイトレワールドに追加。
-		//	g_renderingEngine->AddModelToRaytracingWorld(m_renderToGBufferModel);
-		//	m_addRaytracingWorldModel = &m_renderToGBufferModel;
-		//}
+	void ModelRender::OnRenderShadowMap(RenderContext& rc)
+	{
+		m_shadowModels.Draw(rc);
 	}
 
 
 	void ModelRender::InitSkeleton(const char* filePath)
 	{
-		/** パスを保存 */
+		/** 一旦tkmのファイルパスを受け取る */
 		std::string skeletonFilePath = filePath;
-		/** ファイルパスの中から ".tkm" という文字を探し、何文字目かを "pos" に保存する */
+		/** パスの中に.tkmが何文字目にあるか探す */
 		int pos = (int)skeletonFilePath.find(".tkm");
-		/** 見つかった位置から4文字分を ".tks" に置き換える */
+		/** .tkmを.tksに置き換える */
 		skeletonFilePath.replace(pos, 4, ".tks");
-		/** 書き換えたパスを使って、スケルトンの初期化 */
+		/** char型に変換してInit */
 		m_skeleton.Init(skeletonFilePath.c_str());
 	}
 
 
-	void ModelRender::InitAnimation(AnimationClip* animationClips, int numAnimationClips, EnModelUpAxis enModelUpAxis)
+	void ModelRender::InitAnimation(AnimationClip* animtionClips, int numAnimationClips, EnModelUpAxis enModelUpAxis)
 	{
-		/**
-		 * TODO: アニメーションクリップを配列で渡す
-		 * アロケーションアレイ
-		 */
-
-		 /** アニメーションクリップを保存 */
-		m_animationClips = animationClips;
-		/** アニメーションクリップの数を保存 */
+		m_animationClips = animtionClips;
 		m_numAnimationClips = numAnimationClips;
-		/** アニメーションクリップが存在している場合 */
-		if (m_animationClips) {
-			/** アニメーションを初期化 */
+		if (m_animationClips != nullptr) {
 			m_animation.Init(m_skeleton, m_animationClips, numAnimationClips);
 		}
 	}
 
 
-	void ModelRender::InitComputeAnimatoinVertexBuffer(
-		const char* tkmFilePath,
-		EnModelUpAxis enModelUpAxis)
+	void ModelRender::SetupShaderEntryPointFunc(ModelInitData& modelInitData)
 	{
-		StructuredBuffer* worldMatrxiArraySB = nullptr;
-		if (m_isEnableInstancingDraw) {
-			worldMatrxiArraySB = &m_worldMatrixArraySB;
+		modelInitData.m_vsEntryPointFunc = "VSMain";
+		modelInitData.m_vsSkinEntryPointFunc = "VSMain";
+		/** アニメーションがある場合 */
+		if (m_animationClips != nullptr) {
+			modelInitData.m_vsSkinEntryPointFunc = "VSSkinMain";
 		}
-
-		//m_computeAnimationVertexBuffer.Init(
-		//	tkmFilePath,
-		//	m_skeleton.GetNumBones(),
-		//	m_skeleton.GetBoneMatricesTopAddress(),
-		//	enModelUpAxis,
-		//	m_maxInstance,
-		//	worldMatrxiArraySB
-		//);
 	}
 
 
-	//void ModelRender::InitModelOnRenderGBuffer(RenderingEngine& renderingEngine, const char* tkmFilePath, EnModelUpAxis enModelUpAxis, bool isShadowReciever)
-	//{
-	//	ModelInitData modelInitData;
-	//	modelInitData.m_fxFilePath = "Assets/shader/preProcess/RenderToGBufferFor3DModel.fx";
-
-	//	/** 頂点シェーダーのエントリーポイントをセットアップ */
-	//	SetupVertexShaderEntryPointFunc(modelInitData);
-	//	/** 頂点の事前計算処理を使う */
-	//	modelInitData.m_computedAnimationVertexBuffer = &m_computeAnimationVertexBuffer;
-	//	if (m_animationClips != nullptr) {
-	//		/** スケルトンを指定する */
-	//		modelInitData.m_skeleton = &m_skeleton;
-	//	}
-
-	//	if (isShadowReciever) {
-	//		modelInitData.m_psEntryPointFunc = "PSMainShadowReciever";
-	//	}
-	//	/** モデルの上方向を指定する */
-	//	modelInitData.m_modelUpAxis = enModelUpAxis;
-
-	//	modelInitData.m_tkmFilePath = tkmFilePath;
-	//	modelInitData.m_colorBufferFormat[0] = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	//	modelInitData.m_colorBufferFormat[1] = DXGI_FORMAT_R8G8B8A8_SNORM;
-	//	modelInitData.m_colorBufferFormat[2] = DXGI_FORMAT_R8G8B8A8_UNORM;
-
-	//	if (m_isEnableInstancingDraw) {
-	//		/** インスタンシング描画を行う場合は、拡張SRVにインスタンシング描画用のデータを設定する */
-	//		modelInitData.m_expandShaderResoruceView[0] = &m_worldMatrixArraySB;
-	//	}
-	//	m_renderToGBufferModel.Init(modelInitData);
-	//}
-
-
-	//void ModelRender::SetupVertexShaderEntryPointFunc(ModelInitData& modelInitData)
-	//{
-	//	modelInitData.m_vsSkinEntryPointFunc = "VSMainUsePreComputedVertexBuffer";
-	//	modelInitData.m_vsEntryPointFunc = "VSMainUsePreComputedVertexBuffer";
-
-	//	if (m_animationClips != nullptr) {
-	//		// アニメーションあり。
-	//		modelInitData.m_vsSkinEntryPointFunc = "VSMainSkinUsePreComputedVertexBuffer";
-	//	}
-	//}
-
-
-	//void ModelRender::InitModelOnZprepass(
-	//	RenderingEngine& renderingEngine,
-	//	const char* tkmFilePath,
-	//	EnModelUpAxis modelUpAxis
-	//)
-	//{
-	//	ModelInitData modelInitData;
-	//	modelInitData.m_tkmFilePath = tkmFilePath;
-	//	modelInitData.m_fxFilePath = "Assets/shader/preProcess/ZPrepass.fx";
-	//	modelInitData.m_modelUpAxis = modelUpAxis;
-
-	//	// 頂点シェーダーのエントリーポイントをセットアップ。
-	//	SetupVertexShaderEntryPointFunc(modelInitData);
-	//	// 頂点の事前計算処理を使う。
-	//	modelInitData.m_computedAnimationVertexBuffer = &m_computeAnimationVertexBuffer;
-
-	//	if (m_animationClips != nullptr) {
-	//		//スケルトンを指定する。
-	//		modelInitData.m_skeleton = &m_skeleton;
-	//	}
-
-	//	modelInitData.m_colorBufferFormat[0] = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	//	if (m_isEnableInstancingDraw) {
-	//		// インスタンシング描画を行う場合は、拡張SRVにインスタンシング描画用のデータを設定する。
-	//		modelInitData.m_expandShaderResoruceView[0] = &m_worldMatrixArraySB;
-	//	}
-
-	//	m_zprepassModel.Init(modelInitData);
-	//}
-
-
-	//void ModelRender::InitModelOnShadowMap(
-	//	RenderingEngine& renderingEngine,
-	//	const char* tkmFilePath,
-	//	EnModelUpAxis modelUpAxis,
-	//	bool isFrontCullingOnDrawShadowMap
-	//)
-	//{
-	//	ModelInitData modelInitData;
-	//	modelInitData.m_tkmFilePath = tkmFilePath;
-	//	modelInitData.m_modelUpAxis = modelUpAxis;
-	//	modelInitData.m_cullMode = isFrontCullingOnDrawShadowMap ? D3D12_CULL_MODE_FRONT : D3D12_CULL_MODE_BACK;
-	//	// 頂点シェーダーのエントリーポイントをセットアップ。
-	//	SetupVertexShaderEntryPointFunc(modelInitData);
-
-	//	// 頂点の事前計算処理を使う。
-	//	modelInitData.m_computedAnimationVertexBuffer = &m_computeAnimationVertexBuffer;
-
-	//	if (m_animationClips != nullptr) {
-	//		//スケルトンを指定する。
-	//		modelInitData.m_skeleton = &m_skeleton;
-	//	}
-
-	//	modelInitData.m_fxFilePath = "Assets/shader/preProcess/DrawShadowMap.fx";
-	//	if (g_renderingEngine->IsSoftShadow()) {
-	//		modelInitData.m_colorBufferFormat[0] = g_softShadowMapFormat.colorBufferFormat;
-	//	}
-	//	else {
-	//		modelInitData.m_colorBufferFormat[0] = g_hardShadowMapFormat.colorBufferFormat;
-	//	}
-
-	//	if (m_isEnableInstancingDraw) {
-	//		// インスタンシング描画を行う場合は、拡張SRVにインスタンシング描画用のデータを設定する。
-	//		modelInitData.m_expandShaderResoruceView[0] = &m_worldMatrixArraySB;
-	//	}
-
-	//	for (int ligNo = 0; ligNo < /*MAX_DIRECTIONAL_LIGHT*/NUM_SHADOW_LIGHT; ligNo++)
-	//	{
-	//		ConstantBuffer* cameraParamCBArray = m_drawShadowMapCameraParamCB[ligNo];
-	//		Model* shadowModelArray = m_shadowModels[ligNo];
-	//		for (int shadowMapNo = 0; shadowMapNo < NUM_SHADOW_MAP; shadowMapNo++) {
-	//			cameraParamCBArray[shadowMapNo].Init(sizeof(Vector4), nullptr);
-	//			modelInitData.m_expandConstantBuffer = &cameraParamCBArray[shadowMapNo];
-	//			modelInitData.m_expandConstantBufferSize = sizeof(Vector4);
-	//			shadowModelArray[shadowMapNo].Init(modelInitData);
-	//		}
-	//	}
-	//}
+	void ModelRender::UpdateWorldMatrixInModes()
+	{
+		m_model.UpdateWorldMatrix(m_pos, m_rot, m_sca);
+		m_shadowModels.UpdateWorldMatrix(m_pos, m_rot, m_sca);
+	}
 
 
 	void ModelRender::Update()
 	{
-		// アニメーションの更新などをここで行う
-		// 例: m_animation.Progress(g_gameTime->GetFrameDeltaTime());
+		/** モデルのワールド行列を更新する */
+		UpdateWorldMatrixInModes();
 
-		// とりあえず今は空でもOK
+		/** スケルトンのボーン行列を更新する */
+		if (m_skeleton.IsInited()) {
+			m_skeleton.Update(m_model.GetWorldMatrix());
+		}
+
+		/** アニメーションを進める */
+		m_animation.Progress(g_gameTime->GetFrameDeltaTime() * m_animationSpeed);
 	}
 
 
 	void ModelRender::Draw(RenderContext& rc)
 	{
-		// ここで描画コマンドを発行する
-
-		// まだ実装していないなら空でもOK
+		g_renderingEngine->RegisterModel(&m_model);
+		g_renderingEngine->AddRenderObject(this);
 	}
 }
--- BeastEngine/graphics/ModelRender.h
+++ BeastEngine/graphics/ModelRender.h
@@ -1,11 +1,9 @@
 ﻿/**
  * @file ModelRender.h
- * @brief モデルレンダーのヘッダファイル
+ * @brief モデルレンダー
  * @author 竹林尚哉
  */
 #pragma once
- //#include "graphics/ComputeAnimationVertexBuffer.h"
-#include "graphics/IRenderer.h"  
 
 
 namespace nsBeastEngine
@@ -16,104 +14,187 @@ namespace nsBeastEngine
 	class ModelRender : public IRenderer
 	{
 	public:
-		ModelRender();
-		~ModelRender();
+		/**
+		 * @brief アニメーションが再生中か
+		 * @return 再生中ならtrue
+		 */
+		inline bool IsPlayingAnimation() const { return m_animation.IsPlaying(); }
 
+		/**
+		 * @brief 位置、回転、大きさの設定
+		 * @param pos 位置
+		 * @param rot 回転
+		 * @param sca 大きさ
+		 */
+		inline void SetTRS(const Vector3& pos, const Quaternion& rot, const Vector3& sca)
+		{
+			m_pos = pos;
+			m_rot = rot;
+			m_sca = sca;
+		}
+
+		/**
+		 * @brief 位置の設定
+		 *@param pos 位置
+		 */
+		inline void SetPosition(const Vector3& pos) { m_pos = pos; }
+		/**
+		 * @brief 位置の設定
+		 * @param x x座標
+		 * @param y y座標
+		 * @param z z座標
+		 */
+		inline void SetPosition(const float& x, const float& y, const float& z) { m_pos = Vector3(x, y, z); }
+
+		/**
+		 * @brief 回転の設定
+		 * @param rot 回転
+		 */
+		inline void SetRotation(const Quaternion& rot) { m_rot = rot; }
+
+		/**
+		 * @brief 大きさの設定
+		 * @param sca 大きさ
+		 */
+		inline void SetScale(const Vector3& sca) { m_sca = sca; }
+		/**
+		 * @brief 大きさの設定
+		 * @param x x方向の大きさ
+		 * @param y y方向の大きさ
+		 * @param z z方向の大きさ
+		 */
+		inline void SetScale(const float& x, const float& y, const float& z) { m_sca = Vector3(x, y, z); }
+
+		/**
+		 * @brief モデルの取得
+		 * @return モデル
+		 */
+		inline Model& GetModel() { return m_model; }
+
+
+	public:
+		ModelRender()
+			: m_pos(Vector3::Zero)
+			, m_sca(Vector3::One)
+			, m_rot(Quaternion::Identity)
+			, m_animationClips(nullptr)
+			, m_maxInstance(1)
+			, m_numAnimationClips(0)
+			, m_animationSpeed(1.0f)
+		{
+		};
+		~ModelRender() = default;
+
+		/**
+		 * @brief モデルの初期化用関数
+		 * @param filePath			ファイルパス
+		 * @param animationClips	アニメーションクリップ
+		 * @param numAnimationClips アニメーションの数
+		 * @param islighting		ライティングの有効/無効
+		 * @param enModelUpAxis		モデルの上方向
+		 */
 		void Init(
 			const char* filePath,
-			AnimationClip* animationClips = nullptr,
+			AnimationClip* animationeClips = nullptr,
 			int numAnimationClips = 0,
-			EnModelUpAxis enModelUpAxis = enModelUpAxisZ,
-			bool isShadowReciever = true,
-			int maxInstance = 1,
-			bool isFrontCullingOnDrawShadowMap = false
+			bool islighting = true,
+			EnModelUpAxis enModelUpAxiz = enModelUpAxisZ
 		);
 
+		/**
+		 * @brief アニメーションの再生
+		 * @param animNo			アニメーションクリップの番号
+		 * @param interpolateTime	補完時間(単位：秒)
+		 */
+		void PlayAnimation(int animNo, float interpolateTime = 0.0f)
+		{
+			m_animation.Play(animNo, interpolateTime);
+		}
 
-		void Update();
-
+		/**
+		 * @brief 描画処理
+		 * @param rc レンダリングコンテキスト
+		 */
 		void Draw(RenderContext& rc);
+
+		/**
+		 * @brief アニメーションイベントリスナーの登録
+		 * @param eventListener 登録するリスナー
+		 */
+		void AddAnimationEvent(AnimationEventListener eventListener)
+		{
+			m_animation.AddAnimationEventListener(eventListener);
+		}
+
+		/**
+		 * @brief 更新
+		 */
+		void Update();
 
 
 	private:
 		/**
-		 * @brief スケルトンの初期化
-		 * @param filePath	モデルファイルのパス
+		 * @brief シャドウマップへの描画パスから呼ばれる処理
+		 * @param rc レンダリングコンテキスト
+		 */
+		void OnRenderShadowMap(RenderContext& rc) override;
+
+		/**
+		 * @brief スケルトンの初期化用関数
+		 * @param filePath ファイルパス
 		 */
 		void InitSkeleton(const char* filePath);
 
 		/**
-		 * @brief アニメーションの初期化
-		 * @param animationClips	アニメーションクリップの配列
-		 * @param numAnimationClips アニメーションクリップの数
-		 * @param enModelUpAxis		モデルの上方向の軸
+		 * @brief アニメーションの初期化用関数
+		 * @param animtionClips		アニメーションクリップ
+		 * @param numAnimationClips アニメーションの数
+		 * @param enModelUpAxis		モデルの上方向
 		 */
-		void InitAnimation(AnimationClip* animationClips, int numAnimationClips, EnModelUpAxis enModelUpAxis);
+		void InitAnimation(
+			AnimationClip* animtionClips,
+			int numAnimationClips,
+			EnModelUpAxis enModelUpAxis
+		);
 
 		/**
-		 * @brief アニメーション頂点バッファの計算処理の初期化
-		 * @param tkmFilePath	tkmファイルのファイルパス
-		 * @param enModelUpAxis モデルの上方向の軸
+		 * @brief アニメーション頂点バッファの初期化用関数
+		 * @param tkmFilePath		ファイルパス
+		 * @param enModelUpAxis	モデルの上方向
 		 */
-		void InitComputeAnimatoinVertexBuffer(const char* tkmFilePath, EnModelUpAxis enModelUpAxis);
+		void InitComputeAnimatoinVertexBuffer(
+			const char* tkmFilePath,
+			EnModelUpAxis enModelUpAxis
+		);
 
 		/**
-		 * @brief GBuffer描画用のモデルの初期化
-		 * @param renderingEngine	レンダリングエンジン
-		 * @param filePath			tkmファイルパス
-		 * @param enModelUpAxis		モデルの上方向の軸
-		 * @param isShadowReciever	シャドウレシーバーかどうか
+		 * @brief シェーダーのエントリーポイントの設定
+		 * @param modelInitData モデルの初期化データ
 		 */
-		void InitModelOnRenderGBuffer(RenderingEngine& renderingEngine, const char* tkmFilePath, EnModelUpAxis enModelUpAxis, bool isShadowReciever);
+		void SetupShaderEntryPointFunc(ModelInitData& modelInitData);
 
 		/**
-		 * @brief 各種モデルの頂点シェーダーのエントリーポイントを設定
-		 * @param modelInitData	モデルの初期化データ
+		 * @brief 各種モデルのワールド行列の更新
 		 */
-		void SetupVertexShaderEntryPointFunc(ModelInitData& modelInitData);
-
-		/**
-		 * @brief ZPrepass描画用のモデルの初期化
-		 * @param renderingEngine	レンダリングエンジン
-		 * @param tkmFilePath		tkmファイルのファイルパス
-		 * @param modelUpAxis		モデルの上方向の
-		 */
-		void InitModelOnZprepass(RenderingEngine& renderingEngine, const char* tkmFilePath, EnModelUpAxis modelUpAxis);
-
-		/**
-		 * @brief シャドウマップ描画用のモデルの初期化
-		 * @param renderingEngine				レンダリングエンジン
-		 * @param tkmFilePath					tkmファイルパス
-		 * @param modelUpAxis					モデルの上方向の軸
-		 * @param isFrontCullingOnDrawShadowMap シャドウマップ描画時にフロントカリングするかどうか
-		 */
-		void InitModelOnShadowMap(RenderingEngine& renderingEngine, const char* tkmFilePath, EnModelUpAxis modelUpAxis, bool isFrontCullingOnDrawShadowMap);
+		void UpdateWorldMatrixInModes();
 
 
 	private:
-		/** スケルトン */
-		Skeleton m_skeleton;
 
-		/** アニメーションクリップ */
-		AnimationClip* m_animationClips;
-		/** アニメーションクリップの数 */
-		int m_numAnimationClips;
-		/** アニメーション */
-		Animation m_animation;
+		Vector3			m_pos = Vector3::Zero;			//位置
+		Vector3			m_sca = Vector3::One;			//大きさ
+		Quaternion		m_rot = Quaternion::Identity;	//回転
 
-		/** インスタンシング描画が有効かどうか */
-		bool m_isEnableInstancingDraw;
-		/** ワールド行列の配列のストラクチャードバッファ */
-		StructuredBuffer m_worldMatrixArraySB;
-		/** アニメーション済み頂点バッファの計算処理 */
-		//ComputeAnimationVertexBuffer m_computeAnimationVertexBuffer;
-		/** 最大インスタンス数 */
-		int	m_maxInstance;
+		Model			m_model;					//モデル
+		Model			m_shadowModels;
 
-		/** RenderToGBufferで描画されるモデル */
-		Model m_renderToGBufferModel;
+		Skeleton		m_skeleton;					//ボーン
+		Animation		m_animation;				//アニメーション
+		AnimationClip* m_animationClips = nullptr;	//アニメーションクリップ
+		ConstantBuffer	m_drawShadowMapCameraParamCB;
 
-		/** ZPrepassで描画されるモデル */
-		Model m_zprepassModel;
+		int				m_maxInstance = 1;			// 最大インスタンス数。
+		int				m_numAnimationClips = 0;	//アニメーションクリップの数
+		float			m_animationSpeed = 1.0f;	//アニメーションの再生速度
 	};
 }
