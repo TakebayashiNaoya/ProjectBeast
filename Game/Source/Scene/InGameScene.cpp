819ba4e211854ac59e56315bb645b760e28cbecd
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-03-03 09:48:41 +0900

    InGameSceneの修正

--- Game/Source/Scene/InGameScene.cpp
+++ Game/Source/Scene/InGameScene.cpp
@@ -6,30 +6,22 @@
 #include "stdafx.h"
 #include "InGameScene.h"
 #include "ResultScene.h"
-#include "Source/Actor/Character/Player/Player.h"
-#include "Source/Core/ParameterManager.h"
-
 
 
 namespace app
 {
 	InGameScene::InGameScene()
 	{
-		core::ParameterManager::CreateInstance();
 	}
 
 
 	InGameScene::~InGameScene()
 	{
-		delete m_player;
-		m_player = nullptr;
 	}
 
 
 	bool InGameScene::Start()
 	{
-		m_player = new actor::Player();
-		m_player->StartWrapper();
 		return true;
 	}
 
@@ -40,13 +32,11 @@ namespace app
 		{
 			m_nextScene = true;
 		}
-		m_player->UpdateWrapper();
 	}
 
 
 	void InGameScene::Render(RenderContext& rc)
 	{
-		m_player->RenderWrapper(rc);
 	}
 
 
