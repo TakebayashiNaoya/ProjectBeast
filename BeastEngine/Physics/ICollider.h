e19bd59c006e892efef69298b6ab726992f75822
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-03-04 12:16:26 +0900

    Add: ICollider コライダー用のインターフェースクラスを追加

---
+++ BeastEngine/Physics/ICollider.h
@@ -0,0 +1,24 @@
+﻿/**
+ * @brief コライダーのインターフェースクラス
+ */
+#pragma once
+
+
+ /** 前方宣言 */
+class btCollisionShape;
+
+
+namespace nsK2EngineLow
+{
+	/**
+	 * @brief コライダーのインターフェースクラス
+	 */
+	class ICollider : public Noncopyable
+	{
+	public:
+		/**
+		 * @brief コライダーを取得
+		 */
+		virtual btCollisionShape* GetBody() const = 0;
+	};
+}
