c1a4222e788601b6916aa22ad32a36c36824daf4
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-03-04 13:43:13 +0900

    Add: BoxColliderを追加

---
+++ BeastEngine/MyPhysics/BoxCollider.cpp
@@ -0,0 +1,19 @@
+﻿/**
+ * @file BoxCollider.cpp
+ * @brief ボックスコライダーの実装
+ * @author 竹林尚哉
+ */
+#include "BeastEnginePreCompile.h"
+#include "Physics.h"
+#include "BoxCollider.h"
+
+
+namespace nsBeastEngine
+{
+	void BoxCollider::Create(const Vector3& size)
+	{
+		/** BulletのbtBoxShapeは半分のサイズを指定する必要があるため、0.5倍して渡す */
+		shape = std::make_unique<btBoxShape>(btVector3(size.x * 0.5f, size.y * 0.5f, size.z * 0.5f));
+	}
+}
+
---
+++ BeastEngine/MyPhysics/BoxCollider.h
@@ -0,0 +1,36 @@
+﻿/**
+ * @file BoxCollider.h
+ * @brief ボックスコライダー
+ * @author 竹林尚哉
+ */
+#pragma once
+#include "ICollider.h"
+
+
+namespace nsBeastEngine
+{
+	/**
+	 * @brief ボックスコライダー。
+	 */
+	class BoxCollider : public ICollider
+	{
+	public:
+		/**
+		 * @brief 生成
+		 */
+		void Create(const Vector3& size);
+
+		/**
+		 * @brief コライダーを取得
+		 */
+		btCollisionShape* GetBody() const override
+		{
+			return shape.get();
+		}
+
+
+	private:
+		/** 当たり判定用ボックス */
+		std::unique_ptr<btBoxShape>	shape;
+	};
+}
