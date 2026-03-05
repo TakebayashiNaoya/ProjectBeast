fbe7e3240a61be6991c7b583d6d58f49aae2925c
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-03-05 14:46:45 +0900

    親ペンギンのクラスを追加
    ・DaddyPenguin
    ・DaddyPenguinIState
    ・DaddyPenguinParameter
    ・DaddyPenguinStateMachine
    ・DaddypenguinStatus

---
+++ Game/Source/Actor/Character/DaddyPenguin/DaddyPenguinParameter.cpp
@@ -0,0 +1,16 @@
+﻿/**
+ * @file DaddyPenguinParameter.cpp
+ * @brief 親ペンギンのパラメーター管理
+ * @author 藤谷
+ */
+#include "stdafx.h"
+#include "DaddyPenguinParameter.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+
+	}
+}
