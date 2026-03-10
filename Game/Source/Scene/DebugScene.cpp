029862d9c4e9247dcf85000b780c49cf39d7414b
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-03-10 13:46:27 +0900

    Add: 海の実装

--- Game/Source/Scene/DebugScene.cpp
+++ Game/Source/Scene/DebugScene.cpp
@@ -21,6 +21,15 @@ namespace app
 
 	bool DebugScene::Start()
 	{
+		NewGO<Ocean>(0);
+		g_camera3D->SetPosition({ 0.0f, 100.0f, -200.0f }); /** 手前・上に配置 */
+		EffectEngine::GetInstance()->ResistEffect(0, u"Assets/effect/test/test.efk");
+		EffectEmitter* m_effect = NewGO<EffectEmitter>(0);
+		m_effect->Init(0);
+		m_effect->SetPosition(Vector3::Zero);
+		m_effect->SetRotation(Quaternion::Identity);
+		m_effect->SetScale({ 15.0f,15.0f,15.0f });
+		m_effect->Play();
 		return true;
 	}
 
