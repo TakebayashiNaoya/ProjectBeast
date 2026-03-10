4b0a2682f3ca2e538c49618a016a62bb7ba53af0
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-03-10 13:58:36 +0900

    Status以外で実装できそうな所の実装

--- Game/Source/Actor/Character/Enemy/Enemy.cpp
+++ Game/Source/Actor/Character/Enemy/Enemy.cpp
@@ -6,6 +6,7 @@
 #include "stdafx.h"
 #include "Enemy.h"
 #include "EnemyStateMachine.h"
+#include "EnemyStatus.h"
 
 
 namespace app
@@ -15,7 +16,7 @@ namespace app
 		Enemy::Enemy()
 		{
 			m_stateMachine = std::make_unique<EnemyStateMachine>(this);
-
+			m_status = std::make_unique<EnemyStatus>();
 			m_status->Setup();
 		}
 
@@ -28,6 +29,8 @@ namespace app
 
 		void Enemy::Update()
 		{
+			m_stateMachine->Update();
+
 			CharacterBase::Update();
 		}
 
--- Game/Source/Actor/Character/Enemy/EnemyIState.cpp
+++ Game/Source/Actor/Character/Enemy/EnemyIState.cpp
@@ -5,6 +5,7 @@
  */
 #include "stdafx.h"
 #include "EnemyIState.h"
+#include "EnemyStateMachine.h"
 
 
 namespace app
@@ -56,11 +57,20 @@ namespace app
 
 		void EnemyWanderingState::Update()
 		{
+			if (m_owner->GetStickLAmount() < 0.01f) {
+				return;
+			}
+
+			const Vector3& moveDirection = m_owner->GetDirection();
+			// NOTE:Statusの設定が出来たらコメントを解除する
+			//const Vector3 move = m_owner->GetOwnerStatus()->GetSpeed();
+			//m_owner->SetMoveVector(move);
 		}
 
 
 		void EnemyWanderingState::Exit()
 		{
+			m_owner->SetMoveVector(Vector3::Zero);
 		}
 
 
@@ -83,13 +93,26 @@ namespace app
 
 		void EnemyChaceState::Update()
 		{
+			if (m_owner->GetStickLAmount() < 0.01f) {
+				return;
+			}
 
+			const Vector3& moveDirection = m_owner->GetDirection();
+
+			// NOTE:Statusの設定が出来たらコメントを解除する
+			//const float moveSpeed = m_owner->GetOwnerStatus()->GetSpeed();
+			//const float dashSpeed = m_owner->GetOwnerStatus()->GetDashSpeed();
+			//const float moveDashSpeed = moveSpeed * dashSpeed;
+
+			//const Vector3 move = moveDirection * moveDashSpeed;
+
+			//m_owner->SetMoveVector(move);
 		}
 
 
 		void EnemyChaceState::Exit()
 		{
-
+			m_owner->SetMoveVector(Vector3::Zero);
 		}
 
 
