a8b267efacff556d0f9de66482165fcbdf48de64
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-03-11 11:12:56 +0900

    子ペンギンの基底クラスを追加

---
+++ Game/Source/Actor/Character/Penguin/ChildPenguin/ChildPenguinBase.cpp
@@ -0,0 +1,48 @@
+﻿/**
+ * @file ChildPenguinBase.cpp
+ * @brief 子ペンギンの基底クラス
+ * @author 藤谷
+ */
+#include "stdafx.h"
+#include "ChildPenguinBase.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+
+		void ChildPenguinBase::Start()
+		{
+			// ペンギンの初期化
+			PenguinBase::Init(PenguinBase::EnPenguinType::Child);
+			// 基底クラスのStartを呼び出す
+			PenguinBase::Start();
+		}
+
+
+		void ChildPenguinBase::Update()
+		{
+			PenguinBase::Update();
+		}
+
+
+		void ChildPenguinBase::Render(RenderContext& rc)
+		{
+			PenguinBase::Render(rc);
+		}
+
+
+		void ChildPenguinBase::Init(const EnChildPenguinType childPenguinType)
+		{
+			// 子ペンギンの種類を保存
+			m_childPenguinType = childPenguinType;
+		}
+
+
+		ChildPenguinBase::ChildPenguinBase()
+			: m_childPenguinType(EnChildPenguinType::Max)
+		{
+		}
+	}
+}
---
+++ Game/Source/Actor/Character/Penguin/ChildPenguin/ChildPenguinBase.h
@@ -0,0 +1,62 @@
+﻿/**
+ * @file ChildPenguinBase.h
+ * @brief 子ペンギンの基底クラス
+ * @author 藤谷
+ */
+#pragma once
+#include "Source/Actor/Character/Penguin/PenguinBase.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+		/**
+		 * @brief 子ペンギンの基底クラス
+		 */
+		class ChildPenguinBase : public PenguinBase
+		{
+		public:
+			/**
+			 * @brief 子ペンギンの種類
+			 */
+			enum class EnChildPenguinType : uint8_t
+			{
+				// 世話焼き
+				Caring,
+				// おちょこちょい
+				Clumsy,
+				// 真面目
+				Serious,
+				// 甘えん坊
+				Spoiled,
+				Max
+			};
+
+
+		protected:
+			virtual void Start() override;
+			virtual void Update() override;
+			virtual void Render(RenderContext& rc) override;
+
+
+		protected:
+			/**
+			 * @brief 初期化
+			 * @param childPenguinType 子ペンギンの種類
+			 */
+			void Init(const EnChildPenguinType childPenguinType);
+
+
+		public:
+			ChildPenguinBase();
+			virtual ~ChildPenguinBase() override = default;
+
+
+		protected:
+			/** 子ペンギンの種類 */
+			EnChildPenguinType m_childPenguinType;
+		};
+	}
+}
+
