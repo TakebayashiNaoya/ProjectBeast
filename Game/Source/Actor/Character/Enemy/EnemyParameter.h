3b7cf93d7e4924d4332a2dcd88c091ff8e8a343a
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-03-11 11:01:51 +0900

    パラメーターとステータスの実装

---
+++ Game/Source/Actor/Character/Enemy/EnemyParameter.h
@@ -0,0 +1,33 @@
+﻿/**
+ * @file EnemyParameter.h
+ * @brief エネミーのパラメーター管理
+ * @author 立山
+ */
+#pragma once
+#include "Source/Actor/Character/CharacterParameter.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+		/**
+		 * @brief エネミーパラメーター
+		 * @details エネミー固有のパラメーターを保持する
+		 */
+		struct MasterEnemyParameter : public MasterCharacterParameter
+		{
+			appParameter(MasterEnemyParameter);
+
+#ifdef APP_PARAM_HOT_RELOAD
+			void Load(const nlohmann::json& j) override
+			{
+				load(j, *this);
+			}
+#endif // APP_PARAM_HOT_RELOAD
+
+			// エネミー固有のパラメーターをここに追加していく
+		};
+	}
+}
+
--- Game/Source/Actor/Character/Enemy/EnemyStatus.cpp
+++ Game/Source/Actor/Character/Enemy/EnemyStatus.cpp
@@ -4,6 +4,7 @@
  * @author 立山
  */
 #include "stdafx.h"
+#include "EnemyParameter.h"
 #include "EnemyStatus.h"
 #include "Source/Core/ParameterManager.h"
 
@@ -14,19 +15,36 @@ namespace app
 	{
 		EnemyStatus::EnemyStatus()
 		{
-
+			// 外部ファイルを読み込み
+			core::ParameterManager::Get()->LoadParameter<MasterEnemyParameter>("Assets/parameter/player/PlayerParameter.json", [](const nlohmann::json& j, MasterEnemyParameter& parameter)
+				{
+					parameter.maxHp = j["maxHp"].get<int>();
+					parameter.hp = j["hp"].get<int>();
+					parameter.walkSpeed = j["walkSpeed"].get<float>();
+					parameter.runSpeed = j["runSpeed"].get<float>();
+					parameter.radius = j["radius"].get<float>();
+					parameter.height = j["height"].get<float>();
+				});
 		}
 
 
 		EnemyStatus::~EnemyStatus()
 		{
-
+			// 使用終了
+			core::ParameterManager::Get()->UnloadParameter<MasterEnemyParameter>();
 		}
 
 
 		void EnemyStatus::Setup()
 		{
-
+			// 読み込んだパラメーター取得
+			const auto* parameter = core::ParameterManager::Get()->GetParameter<MasterEnemyParameter>();
+			m_maxHp = parameter->maxHp;
+			m_hp = parameter->hp;
+			m_walkSpeed = parameter->walkSpeed;
+			m_runSpeed = parameter->runSpeed;
+			m_radius = parameter->radius;
+			m_height = parameter->height;
 		}
 
 
