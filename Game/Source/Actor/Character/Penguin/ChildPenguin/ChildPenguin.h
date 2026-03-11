3586fe0354a162fd3fd3adce54d042ef9ab78779
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-03-11 15:30:42 +0900

    Fujitani/create child penguin animation (#67)
    * Characterのクラスの改装にPenguinを追加
    
    親ペンギンのクラスをCharacter/Penguin/DaddyPenguinへ移動
    
    * ペンギンのtkaファイルを移動
    
    * ペンギンの基底クラスを追加
    
    * ペンギンの基底クラスを実装したので、親ペンギンの継承関係を修正
    
    * 子ペンギンの基底クラスを追加
    
    * 子ペンギンの派生クラスを追加
    
    世話焼き、おっちょこちょい、真面目、甘えん坊
    
    * 子ペンギン用のパラメータファイルを作成
    
    * ファイル、フィルター情報を更新
    
    * 子ペンギンの派生クラスを削除
    
    * 子ペンギンクラスの名前を変更
    
    * 子ペンギンの種類のセッターゲッターを実装
    
    * ペンギンのアニメーションの情報を独自のヘッダに移動
    
    * 子ペンギンのクラスを追加
    
    * ファイル、フィルターの情報を更新

---
+++ Game/Source/Actor/Character/Penguin/ChildPenguin/ChildPenguin.h
@@ -0,0 +1,65 @@
+﻿/**
+ * @file ChildPenguin.h
+ * @brief 子ペンギンクラス
+ * @author 藤谷
+ */
+#pragma once
+#include "Source/Actor/Character/Penguin/PenguinBase.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+		/** 前方宣言 */
+		class ChildPenguinStateMachine;
+
+
+		/**
+		 * @brief 親ペンギンクラス
+		 */
+		class ChildPenguin : public PenguinBase
+		{
+		public:
+			/**
+			 * @brief 子ペンギンのタイプ
+			 */
+			enum class EnChildPenguinType : uint8_t
+			{
+				// 世話焼き
+				Caring = 0,
+				// おっちょこちょい
+				Clumsy,
+				// 甘えん坊
+				Spoiled,
+				// しっかり者
+				Serious,
+			};
+
+
+		public:
+			/**
+			 * @brief ステートマシンを取得
+			 * @return ステートマシンのポインタ
+			 */
+			inline ChildPenguinStateMachine* GetStateMachine() { return m_stateMachine.get(); }
+
+
+		public:
+			ChildPenguin();
+			virtual ~ChildPenguin() override = default;
+
+
+		private:
+			void Start() override final;
+			void Update() override final;
+			void Render(RenderContext& rc) override final;
+
+
+		private:
+			/** ステートマシン */
+			std::unique_ptr<ChildPenguinStateMachine> m_stateMachine;
+		};
+	}
+}
+
