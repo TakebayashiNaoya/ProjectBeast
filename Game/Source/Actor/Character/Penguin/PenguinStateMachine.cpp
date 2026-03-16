1ecf12c878083cf3ff0e1cca7bfb60b085bb7fb3
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-03-16 12:36:41 +0900

    ペンギン共通のステータスクラスを追加

---
+++ Game/Source/Actor/Character/Penguin/PenguinStateMachine.cpp
@@ -0,0 +1,52 @@
+﻿/**
+ * @file PenguinStateMachine.cpp
+ * @brief ペンギンのステートマシン
+ * @author 藤谷
+ */
+#include "stdafx.h"
+#include "PenguinBase.h"
+#include "PenguinStateMachine.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+
+		namespace
+		{
+			/** 重力 */
+			constexpr float GRAVITY = -9.8f;
+		}
+
+
+		void PenguinStateMachine::Jump()
+		{
+			m_currentJumpPower += GRAVITY;
+			const float jumpPower = m_jumpPower + m_currentJumpPower;
+			Vector3 jumpVector = Vector3::Up * jumpPower;
+			Vector3 nextPosition = m_ownerActor->GetTransform().m_position + jumpVector;
+
+			m_ownerActor->SetPosition(nextPosition);
+
+			if (nextPosition.y < 0.0f)
+			{
+				nextPosition.y = 0.0f;
+			}
+		}
+
+
+		PenguinStateMachine::PenguinStateMachine(PenguinBase* ownerPenguinBase)
+			: CharacterStateMachine(ownerPenguinBase)
+			, m_ownerPenguinBase(ownerPenguinBase)
+			, m_currentJumpPower(0.0f)
+			, m_jumpPower(0.0f)
+		{}
+
+
+		core::IState* PenguinStateMachine::GetChangeState()
+		{
+			return nullptr;
+		}
+	}
+}
---
+++ Game/Source/Actor/Character/Penguin/PenguinStateMachine.h
@@ -0,0 +1,71 @@
+﻿/**
+ * @file PenguinStateMachine.h
+ * @brief ペンギンのステートマシン
+ * @author 藤谷
+ */
+#pragma once
+#include "Source/Actor/Character/CharacterStateMachine.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+		/** 前方宣言 */
+		class PenguinBase;
+
+		/**
+		 * @brief ペンギンのステートマシンクラス
+		 */
+		class PenguinStateMachine : public CharacterStateMachine
+		{
+			// ここにペンギン共通のセッター関数を追加していく
+		public:
+			/**
+			 * @brief ジャンプパワーを設定
+			 * @param jumpPower ジャンプパワー
+			 */
+			inline void SetJumpPower(const float jumpPower)
+			{
+				m_jumpPower = jumpPower;
+			}
+
+
+			// ここに親ペンギン共通のゲッター関数を追加していく
+		public:
+			/**
+			 * @brief ジャンプパワーを取得
+			 * @return ジャンプパワー
+			 */
+			inline float GetJumpPower() const
+			{
+				return m_jumpPower;
+			}
+
+
+			virtual core::IState* GetChangeState() override;
+
+
+		public:
+			/**
+			 * @brief ジャンプ処理
+			 */
+			void Jump();
+
+
+		public:
+			PenguinStateMachine(PenguinBase* ownerPenguinBase);
+			~PenguinStateMachine() = default;
+
+
+		private:
+			/** 親ペンギンのポインタ */
+			PenguinBase* m_ownerPenguinBase;
+			/** 現在のジャンプパワー */
+			float m_currentJumpPower;
+			/** ジャンプパワー */
+			float m_jumpPower;
+		};
+	}
+}
+
