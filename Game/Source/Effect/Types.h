487dce4eefe3510138bb878f93ad3112c2150b66
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-03-21 17:02:43 +0900

    エフェクトを出す処理を実装

---
+++ Game/Source/Effect/Types.h
@@ -0,0 +1,34 @@
+﻿/**
+ * @file Types.h
+ * @brief エフェクト用の定数など必要な情報を定義するファイル
+ * @author 藤谷
+ */
+#pragma once
+#include <string>
+
+
+ /** エフェクトの種類 */
+enum class EnEffectKind : uint8_t
+{
+	Kind = 0,
+	DaddyPenguinCommand = Kind,
+	Max,
+	None = Max,
+};
+
+
+/** エフェクトの情報の構造体 */
+struct EffectInformation
+{
+	const char16_t* assetPath;
+	//
+	EffectInformation(const char16_t* path) : assetPath(path) {}
+};
+
+
+/** 情報を保持 */
+static EffectInformation effectInformation[static_cast<uint8_t>(EnEffectKind::Max)] =
+{
+	EffectInformation(u"Assets/effect/DaddyPenguinCommand.efk")
+};
+
