796e2aa62aee982d3ca3ab033c0dd395e08d4949
kutsunaharuki <ca01254@st.kawahara.ac.jp>
2026-03-23 12:31:24 +0900

    DebugSceneの修正

--- Game/Source/Scene/DebugScene.cpp
+++ Game/Source/Scene/DebugScene.cpp
@@ -7,22 +7,6 @@
 #if defined(APP_DEBUG)
 #include "DebugScene.h"
 
-#include "Source/UI/CountDownMenu.h"
-#include "Source/UI/Layout.h"
-#include "TitleScene.h"
-#include "Source/Core/Fade.h"
-#include "Source/UI/InGameTimerMenu.h"
-
-
-namespace
-{
-	app::ui::Layout* layout = nullptr;
-	app::ui::CountDownMenu* countDown = nullptr;
-
-	app::ui::Layout* timerLayout = nullptr;
-	app::ui::InGameTimerMenu* inGameTimer = nullptr;
-}
-
 
 namespace app
 {
@@ -36,44 +20,12 @@ namespace app
 
 	bool DebugScene::Start()
 	{
-		if (!layout)
-		{
-			layout = new ui::Layout();
-			layout->Initialize<ui::CountDownMenu>
-				("Assets/parameter/countDown/CountDown.json");
-			countDown = layout->GetMenu<ui::CountDownMenu>();
-		}
-		if (!timerLayout)
-		{
-			const auto& fade = core::Fade::Get();
-			timerLayout = new ui::Layout();
-			timerLayout->Initialize<ui::InGameTimerMenu>
-				("Assets/parameter/timer/InGameTimer.json");
-			inGameTimer = timerLayout->GetMenu<ui::InGameTimerMenu>();
-		}
 		return true;
 	}
 
 
 	void DebugScene::Update()
 	{
-		const auto& fade = core::Fade::Get();
-		if (countDown && inGameTimer)
-		{
-			if (!fade.IsFadeIn()
-				&& !countDown->IsCountDownStart())
-			{
-				countDown->SetCountDownStartFlag(true);
-			}
-			if (!inGameTimer->IsTimerActive() 
-				&& countDown->IsCountDownFinished())
-			{
-				//countDown->SetCountDownFinishedFlag(true);
-				inGameTimer->StartTimer();
-			}
-		}
-		if (layout)layout->Update();
-		if (timerLayout)timerLayout->Update();
 	}
 
 
@@ -83,8 +35,7 @@ namespace app
 
 	void DebugScene::Render(RenderContext& rc)
 	{
-		if (layout)layout->Render(rc);
-		if (timerLayout)timerLayout->Render(rc);
+		
 	}
 
 
@@ -93,11 +44,6 @@ namespace app
 		if (g_pad[0]->IsTrigger(enButtonA))
 		{
 			id = DebugScene::ID();
-			return true;
-		}
-		if (g_pad[0]->IsTrigger(enButtonX))
-		{
-			id = TitleScene::ID();
 			return true;
 		}
 		return false;
