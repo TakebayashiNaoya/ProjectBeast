ffb8051531c7812b55d0a1196b70b2ca22723383
kutsunaharuki <ca01254007@st.kawahara.ac.jp>
2026-03-24 17:49:34 +0900

    Kutuna/create pb waking up timer (#102)
    * シロクマの起床タイマーのアセットを追加
    
    * シロクマの起床タイマーを追加
    
    30秒経過したら非表示。
    シロクマの上にタイマーを設定。
    
    ---------
    
    Co-authored-by: kutsunaharuki <ca01254@st.kawahara.ac.jp>

---
+++ Game/Source/UI/PBWakingUpTimerMenu.h
@@ -0,0 +1,96 @@
+﻿/**
+ * @file PBWakingUpTimerMenu.h
+ * @brief PB起床タイマーの動的処理クラス
+ * @author 忽那
+ */
+#pragma once
+#include "Menu.h"
+#include "Source/Actor/Character/Enemy/EnemyController.h"
+#include "Source/Actor/Character/Enemy/Enemy.h"
+
+
+namespace app
+{
+	namespace ui
+	{
+		class PBWakingUpTimerDigit
+		{
+		public:
+			PBWakingUpTimerDigit();
+			~PBWakingUpTimerDigit();
+			void Update();
+			void SetUIDigit(UIDigit* digit);
+			void SetValue(float value);
+
+
+		private:
+			UIDigit* m_digit;
+		};
+
+
+
+
+		class PBWakingUpTimerMenu : public MenuBase
+		{
+			using PBWakingUpTimerClass = MenuBase;
+
+
+		public:
+			PBWakingUpTimerMenu();
+			~PBWakingUpTimerMenu();
+			void Update()override;
+			void InitializeLogic()override;
+			void ResetTimer();
+
+
+			/**
+			 * @brief PB起床タイマーの描画距離の取得
+			 * @return m_isLengthDraw 描画距離の取得
+			 */
+			//inline bool IsLengthDraw()const { return m_isLengthDraw; }
+			/**
+			 * @brief PB起床タイマーの描画距離の設定
+			 * @param isLengthDraw 描画距離に応じての設定
+			 */
+			//inline void SetIsLengthDraw(bool isLengthDraw) { m_isLengthDraw = isLengthDraw; }
+
+			/**
+			 * @brief PB起床タイマーのタイマーアクティブの取得
+			 * @return m_isTimerActive タイマーアクティブの取得
+			 */
+			inline bool IsPBTimerActive()const { return m_isPBTimerActive; }
+			/**
+			 * @brief PB起床タイマーのタイマーアクティブかの設定
+			 * @param isTimerActive タイマーアクティブの設定
+			 */
+			inline void SetIsPBTimerActive(bool isPBTimerActive) { m_isPBTimerActive = isPBTimerActive; }
+			/**
+			 * @brief 外部からPBの起床タイマーを設定する
+			 * @param time PBの起床タイマー
+			 */
+			inline void SetCurrentPBTime(float time){ m_currentPBTime = time; }
+			
+			/**
+			 * @brief Enemyの設定
+			 * @param enemy しろくま
+			 */
+			void SetEnemy(actor::Enemy* enemy) { m_enemy = enemy; }
+
+
+		private:
+			float m_currentPBTime;
+			float m_maxTime;
+			float m_minTime;
+			bool m_isLengthDraw;
+			bool m_isPBTimerActive;
+
+			actor::EnemyController* m_eneCon;
+			actor::Enemy* m_enemy;
+
+			using Digit = std::unique_ptr<PBWakingUpTimerDigit>;
+			using Key = uint32_t;
+
+			std::unordered_map<Key, Digit>m_wakingUpTimeMap;
+		};
+	}
+}
