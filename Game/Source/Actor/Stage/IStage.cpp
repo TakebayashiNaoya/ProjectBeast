21559be417762836380490b0842109a31f817a01
tachiyamakotaro <ca01254014@st.kawahara.ac.jp>
2026-03-26 18:41:58 +0900

    Tachiyama/enemy ray (#108)
    * enemyの修正とステージの当たり判定
    
    * アプリケーションにPhysicsWorldの追加
    
    * PhysicsとPhysicalBodyの変更
    
    ---------
    
    Co-authored-by: Tachiyama Kotarou <lishanhutailang3@gmail.com>

--- Game/Source/Actor/Stage/IStage.cpp
+++ Game/Source/Actor/Stage/IStage.cpp
@@ -31,6 +31,13 @@ namespace app
 					m_modelRender.Init(m_pendingModelPath.c_str());
 					m_modelRender.SetTRS(m_transform.m_position, m_transform.m_rotation, m_transform.m_scale);
 					m_modelRender.Update();
+
+					m_physicalObj.CreateFromModel(
+						m_modelRender.GetModel(),
+						m_modelRender.GetModel().GetWorldMatrix(),
+						nsBeastEngine::nsCollision::CollisionAttribute::Ground
+					);
+
 					m_isModelLoaded = true;
 				}
 				return;
