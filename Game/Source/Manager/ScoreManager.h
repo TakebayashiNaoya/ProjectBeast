a419ea35084e35f01e7065edd201e09502d2d8df
tachiyamakotaro <ca01254014@st.kawahara.ac.jp>
2026-03-30 15:16:09 +0900

    Resultで必要になるManager (#120)
    Co-authored-by: Tachiyama Kotarou <lishanhutailang3@gmail.com>

---
+++ Game/Source/Manager/ScoreManager.h
@@ -0,0 +1,71 @@
+﻿/**
+ * @file ScoreManager.h
+ * @brief スコアの管理をするクラス
+ * @author 立山
+ */
+#pragma once
+
+
+namespace app
+{
+	class ScoreManager
+	{
+	public:
+
+
+		void SetCollectedCount(int collected) { m_collectedCount = collected; }
+		int GetCollectedCount() { return m_collectedCount; }
+
+
+		void AddCollectedCount() { m_collectedCount++; }
+		void SubCollectedCount()
+		{
+			m_collectedCount--;
+			m_totalCount--;
+		}
+
+		void SetTotalCount(int total) { m_totalCount = total; }
+		int GetTotalCount() { return m_totalCount; }
+
+		void SubTotalCount() { m_totalCount--; }
+
+
+	public:
+		static void CreateInstance()
+		{
+			if (m_instance == nullptr)
+			{
+				m_instance = new ScoreManager;
+			}
+		}
+
+
+		static ScoreManager& GetInstance()
+		{
+			return *m_instance;
+		}
+
+		static void DestroyInstance()
+		{
+			if (m_instance != nullptr)
+			{
+				delete m_instance;
+				m_instance = nullptr;
+			}
+		}
+
+	private:
+		ScoreManager();
+		~ScoreManager();
+
+	private:
+		int m_collectedCount;
+		int m_totalCount;
+
+
+	private:
+		static ScoreManager* m_instance;
+
+
+	};
+}
