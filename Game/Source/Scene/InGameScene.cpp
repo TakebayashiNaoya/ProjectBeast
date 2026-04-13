66c44bf79bc0f2e0102045a4c4ada58c31452035
竹林 尚哉 <ca01254013@st.kawahara.ac.jp>
2026-04-13 17:17:34 +0900

    ゲームループでエラーが出る現象と、ロード後の一瞬の暗転を修正。 (#170)

--- Game/Source/Scene/InGameScene.cpp
+++ Game/Source/Scene/InGameScene.cpp
@@ -91,12 +91,17 @@ namespace app
 		ScoreManager::DestroyInstance();
 		TimeManager::DestroyInstance();
 
-
+		/** タイトルへ戻る場合は ResultScene を経由しないため、ここで破棄する。
+		 *  リザルト遷移の場合は ResultScene::~ResultScene が担当する。 */
 		if (m_goTitle) {
 			if (app::achievement::AchievementManager::GetInstance()) {
 				app::achievement::AchievementManager::DestroyInstance();
 			}
 		}
+
+		/** 2周目以降のために GameCamera の登録を解除する。
+		 *  次の InGameScene::LoadPhase::Camera で新しいインスタンスを Register できるようにする。 */
+		camera::CameraManager::Get().Unregister(camera::GameCamera::ID());
 	}
 
 
@@ -214,6 +219,7 @@ namespace app
 			m_cameraSteering.SetConfig(config);
 			m_cameraSteering.SetTargetCharacter(m_daddyPenguin);
 
+			/** 2周目以降は Unregister 済みのため、毎回新しいインスタンスを登録できる */
 			auto gameCamera = std::make_shared<camera::GameCamera>();
 			camera::CameraManager::Get().Register(camera::GameCamera::ID(), gameCamera);
 			camera::CameraManager::Get().SwitchCamera(camera::GameCamera::ID());
