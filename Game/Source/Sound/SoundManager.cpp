d4e54826bc09545f5689a00234f2e720e98c0dce
竹林 尚哉 <ca01254013@st.kawahara.ac.jp>
2026-04-14 22:46:39 +0900

    音量調整 (#184)

--- Game/Source/Sound/SoundManager.cpp
+++ Game/Source/Sound/SoundManager.cpp
@@ -21,10 +21,10 @@ namespace app
 
 
 	SoundManager::SoundManager()
-		: m_masterVolume(0.5f)
-		, m_bgmVolume(0.5f)
-		, m_seVolume(0.5f)
-		, m_voiceVolume(0.5f)
+		: m_masterVolume(0.1f)
+		, m_bgmVolume(0.1f)
+		, m_seVolume(0.1f)
+		, m_voiceVolume(0.1f)
 	{
 		m_seList.clear();
 
