6e070e0bcb0d86d40cf1c7993d2c859f97c7e435
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2026-05-15 16:59:11 +0900

    ペンギンのエフェクトのステータス

---
+++ Game/Source/Actor/Character/Penguin/PenguinEffectParameter.h
@@ -0,0 +1,60 @@
+﻿/**
+ * @file PenguinEffectParameter.h
+ * @brief ペンギンのエフェクトパラメーター管理
+ * @author 立山
+ */
+#pragma once
+#include "Source/Core/IMasterParameter.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+		/**
+		 * @brief ペンギンエフェクトパラメーター
+		 * @details ペンギンのエフェクトに関連するパラメーターを保持する
+		 */
+		struct MasterPenguinEffectParameter : public core::IMasterParameter
+		{
+			appParameter(MasterPenguinEffectParameter);
+
+#ifdef APP_PARAM_HOT_RELOAD
+			void Load(const nlohmann::json& j) override
+			{
+				load(j, *this);
+			}
+#endif // APP_PARAM_HOT_RELOAD
+
+			/** 泳ぎエフェクトの基本スケール */
+			Vector3 splashEffectScale;
+
+			/** エフェクト発生位置の前方オフセット */
+			float effectOffsetForward;
+
+			/** 泳ぎエフェクトの再生間隔 */
+			float splashEffectInterval;
+
+			/** 泳ぎエフェクトを出すための最低速度の二乗 */
+			float minMoveVelocitySq;
+
+			/** 泳ぎエフェクトの最小スケールの割合 */
+			float minSplashScaleRatio;
+
+			/** 泳ぎエフェクトの最大スケールの割合 */
+			float maxSplashScaleRatio;
+
+			/** 泳ぎエフェクトのスケールを決めるための最低速度 */
+			float minSpeed;
+
+			/** 泳ぎエフェクトのスケールを決めるための最高速度 */
+			float maxSpeed;
+
+			/** 着地エフェクトのスケール */
+			Vector3 landingEffectScale;
+
+			/** スライドエフェクトのスケール */
+			Vector3 slideEffectScale;
+		};
+	}
+}
---
+++ Game/Source/Actor/Character/Penguin/PenguinEffectStatus.cpp
@@ -0,0 +1,89 @@
+﻿/**
+ * @file PenguinEffectStatus.cpp
+ * @brief ペンギンのエフェクトステータスクラス
+ * @author 立山
+ */
+#include "stdafx.h"
+#include "PenguinEffectParameter.h"
+#include "PenguinEffectStatus.h"
+#include "Source/Core/ParameterManager.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+		namespace
+		{
+			const char* PARAMETER_FILE_PATH = "Assets/parameter/character/penguin/PenguinEffectParameter.json";
+		}
+
+
+		PenguinEffectStatus::PenguinEffectStatus()
+			: m_splashEffectScale(Vector3::One)
+			, m_effectOffsetForward(0.0f)
+			, m_splashEffectInterval(0.0f)
+			, m_minMoveVelocitySq(0.0f)
+			, m_minSplashScaleRatio(0.0f)
+			, m_maxSplashScaleRatio(0.0f)
+			, m_minSpeed(0.0f)
+			, m_maxSpeed(0.0f)
+			, m_landingEffectScale(Vector3::One)
+			, m_slideEffectScale(Vector3::One)
+		{
+			// 外部ファイルを読み込み
+			core::ParameterManager::Get()->LoadParameter<MasterPenguinEffectParameter>(PARAMETER_FILE_PATH, [](const nlohmann::json& j, MasterPenguinEffectParameter& parameter)
+				{
+					parameter.splashEffectScale.x = j["splashEffectScale"]["x"].get<float>();
+					parameter.splashEffectScale.y = j["splashEffectScale"]["y"].get<float>();
+					parameter.splashEffectScale.z = j["splashEffectScale"]["z"].get<float>();
+
+					parameter.effectOffsetForward = j["effectOffsetForward"].get<float>();
+					parameter.splashEffectInterval = j["splashEffectInterval"].get<float>();
+					parameter.minMoveVelocitySq = j["minMoveVelocitySq"].get<float>();
+					parameter.minSplashScaleRatio = j["minSplashScaleRatio"].get<float>();
+					parameter.maxSplashScaleRatio = j["maxSplashScaleRatio"].get<float>();
+					parameter.minSpeed = j["minSpeed"].get<float>();
+					parameter.maxSpeed = j["maxSpeed"].get<float>();
+
+					parameter.landingEffectScale.x = j["landingEffectScale"]["x"].get<float>();
+					parameter.landingEffectScale.y = j["landingEffectScale"]["y"].get<float>();
+					parameter.landingEffectScale.z = j["landingEffectScale"]["z"].get<float>();
+
+					parameter.slideEffectScale.x = j["slideEffectScale"]["x"].get<float>();
+					parameter.slideEffectScale.y = j["slideEffectScale"]["y"].get<float>();
+					parameter.slideEffectScale.z = j["slideEffectScale"]["z"].get<float>();
+				});
+		}
+
+
+		PenguinEffectStatus::~PenguinEffectStatus()
+		{
+			core::ParameterManager::Get()->UnloadParameter<MasterPenguinEffectParameter>();
+		}
+
+
+		void PenguinEffectStatus::Setup()
+		{
+			const auto* parameter = core::ParameterManager::Get()->GetParameter<MasterPenguinEffectParameter>();
+			if (!parameter) return;
+
+			m_splashEffectScale = parameter->splashEffectScale;
+			m_effectOffsetForward = parameter->effectOffsetForward;
+			m_splashEffectInterval = parameter->splashEffectInterval;
+			m_minMoveVelocitySq = parameter->minMoveVelocitySq;
+			m_minSplashScaleRatio = parameter->minSplashScaleRatio;
+			m_maxSplashScaleRatio = parameter->maxSplashScaleRatio;
+			m_minSpeed = parameter->minSpeed;
+			m_maxSpeed = parameter->maxSpeed;
+			m_landingEffectScale = parameter->landingEffectScale;
+			m_slideEffectScale = parameter->slideEffectScale;
+		}
+
+
+		void PenguinEffectStatus::Update()
+		{
+			Setup();
+		}
+	}
+}
