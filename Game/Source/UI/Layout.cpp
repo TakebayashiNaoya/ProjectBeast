35440493587d9b8acf8dd26b989cbd63c1b37737
kutsunaharuki <ca01254@st.kawahara.ac.jp>
2026-06-10 10:23:02 +0900

    幽霊子ペンギンの作成
    幽霊子ペンギンに白い三角巾を被せた
    UIVideoのエラー修正

--- Game/Source/UI/Layout.cpp
+++ Game/Source/UI/Layout.cpp
@@ -146,10 +146,10 @@ void InitializeUIParts(app::ui::UIVideo* video, const nlohmann::json& item)
 	data.loop     = item.value("loop",     false);
 	data.autoPlay = item.value("autoPlay", true);
 
-	const Vector3    position = item.contains("position") ? ParseVector3(item["position"]) : Vector3::Zero;
-	const Vector3    scale    = item.contains("scale")    ? ParseVector3(item["scale"])    : Vector3::One;
-	const Quaternion rotation = item.contains("rotation") ? ParseRotation(item["rotation"].get<float>()) : Quaternion::Identity;
-	const Vector4    color    = item.contains("color")    ? ParseColor(item["color"])      : Vector4::White;
+	const Vector3    position = item.contains("position") ? app::util::JsonConverter::ToVector3(item["position"])      : Vector3::Zero;
+	const Vector3    scale    = item.contains("scale")    ? app::util::JsonConverter::ToVector3(item["scale"])		   : Vector3::One;
+	const Quaternion rotation = item.contains("rotation") ? ParseRotation(item["rotation"].get<float>())			   : Quaternion::Identity;
+	const Vector4    color    = item.contains("color")    ? app::util::JsonConverter::ToVector4(item,"color", false)  : Vector4::White;
 
 	video->Initialize(data);
 	video->m_transform.m_localTransform.m_position = position;
