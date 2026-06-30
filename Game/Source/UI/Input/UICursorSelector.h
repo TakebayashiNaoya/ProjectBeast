30ab41f980ddb2654fe1501246388f20d36ed996
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-06-30 15:23:22 +0900

    選択画面に使用するコントローラーの入力情報を管理するクラスを実装

---
+++ Game/Source/UI/Input/UICursorSelector.h
@@ -0,0 +1,38 @@
+﻿/**
+ * @file UICursorSelector.h
+ * @brief UIカーソル選択を行うクラス
+ * @author 藤谷
+ */
+#pragma once
+#include "Types.h"
+
+
+namespace app
+{
+	namespace ui
+	{
+		class CursorIndexSelector
+		{
+		public:
+			// dirがNoneでなければインデックスを動かしてSEを鳴らす
+			bool TryMove(Direction dir);
+
+
+			inline int Get() const { return m_index; }
+			inline void Set(int v) { m_index = v; }
+
+			inline void Reset() { m_index = 0; }
+
+
+			CursorIndexSelector(int max);
+			~CursorIndexSelector() = default;
+
+
+		private:
+			int m_index;
+			int m_max;
+		};
+	}
+}
+
+
